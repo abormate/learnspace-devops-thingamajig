ab6f9b6933e82b5f1c028cf8803d44585422c300
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-16 10:51:55 +0800

    Update compile-run-docu.java

--- kodekloud-laboratories/java-laboratories-devops-fundamentals/compile-run-docu.java
+++ kodekloud-laboratories/java-laboratories-devops-fundamentals/compile-run-docu.java
@@ -1,3 +1,15 @@
+// for a java written file  "MyClass.java" inside a directory -- with the following source
+/**
+ * Prints Hello World Message
+ *
+ */
+public class MyClass {
+    public static void main(String[] args) {
+         System.out.println("Hello Kodekloud");
+    }
+}
+
+
 // compile build stage
 javac MyClass.java
 
