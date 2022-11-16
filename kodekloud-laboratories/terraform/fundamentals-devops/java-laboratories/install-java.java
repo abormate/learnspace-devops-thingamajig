9cb2c14da43f440e24f35410c5d0049b6a7bdeca
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-16 09:42:44 +0800

    Update install-java.java

--- kodekloud-laboratories/terraform/fundamentals-devops/java-laboratories/install-java.java
+++ kodekloud-laboratories/terraform/fundamentals-devops/java-laboratories/install-java.java
@@ -1,2 +1,5 @@
 sudo curl https://download.java.net/java/GA/jdk13.0.2/d4173c853231432d94f001e99d882ca7/8/GPL/openjdk-13.0.2_linux-x64_bin.tar.gz --output /opt/openjdk-13.0.2_linux-x64_bin.tar.gz
 
+sudo tar -xf /opt/openjdk-13.0.2_linux-x64_bin.tar.gz -C /opt/
+  
+  
