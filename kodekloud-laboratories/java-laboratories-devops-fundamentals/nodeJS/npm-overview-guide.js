6b83f6d13c71f8f8c116ab4457312d14eb297ae2
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-17 09:07:33 +0800

    Update npm-overview-guide.js

--- kodekloud-laboratories/java-laboratories-devops-fundamentals/nodeJS/npm-overview-guide.js
+++ kodekloud-laboratories/java-laboratories-devops-fundamentals/nodeJS/npm-overview-guide.js
@@ -1,8 +1,11 @@
 // to look for what file npm from the public node repo
 npm search file
 
-// to install an NPM file or package 
+// to install an NPM file or package -- this installs the NPM in the pwd (present working directory)
 npm install file
+
+// to install NPM globally
+npm install file -g
 
 // to look at what paths node will look at when searching for an npm dependency
 node -e "console.log(module.paths)"
