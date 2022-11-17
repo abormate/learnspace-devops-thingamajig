22e3e3f0a5578e834584dddee18e33239b7af512
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-17 09:36:17 +0800

    Update install-python-guide-overview.py

--- kodekloud-laboratories/java-laboratories-devops-fundamentals/python/install-python-guide-overview.py
+++ kodekloud-laboratories/java-laboratories-devops-fundamentals/python/install-python-guide-overview.py
@@ -1,1 +1,7 @@
+// python 3 introduced many fixes for problems with python 2. however, apps developed on python2 can't be run on python 3 
+
+// install python on CentOS
+yum install python3 -y
+
+// you can install python2 and python 3 simultaneously on a system
 
