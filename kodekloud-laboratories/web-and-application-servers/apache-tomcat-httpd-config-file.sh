14345522bff30273c08e1ecc629e7e4639ff82c6
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-21 10:35:37 +0800

    Create apache-tomcat-httpd-config-file.sh

---
+++ kodekloud-laboratories/web-and-application-servers/apache-tomcat-httpd-config-file.sh
@@ -0,0 +1,5 @@
+# various configurations can be access or modified via the config file for httpd
+/etc/httpd/conf/httpd.conf
+
+# you need to restart your httpd service when you modify or change settings inside the config file
+service httpd restart
