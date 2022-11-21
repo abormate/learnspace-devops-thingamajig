63191024177a03b667d74c3b619a80bd179fc6ad
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-21 08:43:27 +0800

    Update and rename initialize-repo.sh to initialize-repo-add-commit.sh

---
+++ kodekloud-laboratories/source-control-mgt/initialize-repo-add-commit.sh
@@ -0,0 +1,14 @@
+# create and initialize repo on pwd for this example named "myrepo"
+git init myrepo
+
+# check status for untracked files once inside repo folder
+git status
+
+# cd into repo directory
+cd /home/thor/myrepo
+
+# add untracked file into git
+git add index.html
+
+# commit file into git with a custom commit message -- fill out the commit message with something meaningful and descriptive
+git commit -m 'your commit message'
--- kodekloud-laboratories/source-control-mgt/initialize-repo.sh
+++
@@ -1,5 +0,0 @@
-# create and initialize repo on pwd for this example named "myrepo"
-git init myrepo
-
-# check status for untracked files once inside repo folder
-git status
