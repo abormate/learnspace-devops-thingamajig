ccec56e15882c77feed2a154b02cc542a5859429
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-21 08:41:14 +0800

    Update install-git-on-linux.sh

--- kodekloud-laboratories/source-control-mgt/install-git-on-linux.sh
+++ kodekloud-laboratories/source-control-mgt/install-git-on-linux.sh
@@ -1,5 +1,5 @@
-// check OS or distro of Linux
+# check OS or distro of Linux
 cat /etc/os-release
 
-// install git on linux for example on CentOS
+# install git on linux for example on CentOS
 sudo yum install git -y
