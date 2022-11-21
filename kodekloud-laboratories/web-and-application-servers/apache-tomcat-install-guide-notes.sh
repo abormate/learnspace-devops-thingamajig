0f97a905f4b7206ffa2840c2bdb0695e0359e9af
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-21 10:15:01 +0800

    Update and rename apache-tomcat-guide.sh to apache-tomcat-install-guide-notes.sh

--- kodekloud-laboratories/web-and-application-servers/apache-tomcat-install-guide-notes.sh
+++ kodekloud-laboratories/web-and-application-servers/apache-tomcat-install-guide-notes.sh
@@ -1,3 +1,9 @@
 # How to install Apache HTTPD (Tomcat) on a CentOS Linux distro -- it's already on the default CentOS software repo, so you only need to run the ff:
 yum install httpd -y
 
+# After installation, start httpd service
+service httpd start
+
+# After starting service, verify status of httpd service
+service httpd status
+
