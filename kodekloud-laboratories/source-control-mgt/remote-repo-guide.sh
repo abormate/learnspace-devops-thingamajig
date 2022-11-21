2abd19547767c6687267e386fac28c0bf6905c6c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-21 09:31:50 +0800

    Update remote-repo-guide.sh

--- kodekloud-laboratories/source-control-mgt/remote-repo-guide.sh
+++ kodekloud-laboratories/source-control-mgt/remote-repo-guide.sh
@@ -4,3 +4,5 @@ git clone <url of remote repo like on github>
 # command git pull updates a local repo with changes made to a remote repo without downloading the entire remote repo once again
 git pull
 
+# command to push changes you've made from local repo and into a remote repo where code is centralized
+git push
