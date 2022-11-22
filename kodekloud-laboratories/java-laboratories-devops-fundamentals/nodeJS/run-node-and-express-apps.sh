b960868ebf43738ef38d698a91efeea92f547caa
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-22 09:29:03 +0800

    Update run-node-and-express-apps.sh

--- kodekloud-laboratories/java-laboratories-devops-fundamentals/nodeJS/run-node-and-express-apps.sh
+++ kodekloud-laboratories/java-laboratories-devops-fundamentals/nodeJS/run-node-and-express-apps.sh
@@ -4,3 +4,8 @@ npm install
 # run with node the starting point of an app -- for example for an app named "sample_app.js"
 node sample_app.js
 
+# when you need different scripts to initialize different environments for an app -- like for example, a different set of settings for when you're testing, as opposed to when you're in development or when you're in production
+npm run start
+
+# the "start" at the bash command above this line, is a fragment of the package.json file inside the app directory that initilizes a specific number of other scripts to run
+
