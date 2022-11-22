92c1c8e4ddcd18e3db3413c98daac132968d461a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-22 09:23:47 +0800

    Create run-node-and-express-apps.sh

---
+++ kodekloud-laboratories/java-laboratories-devops-fundamentals/nodeJS/run-node-and-express-apps.sh
@@ -0,0 +1,6 @@
+# install list of dependencies inside list of package.json file inside express app package 
+npm install
+
+# run with node the starting point of an app -- for example for an app named "sample_app.js"
+node sample_app.js
+
