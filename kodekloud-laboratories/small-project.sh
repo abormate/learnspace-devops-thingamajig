fa55988dc1b364bf81fb139aaeb4c4bdf849dcb3
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-22 11:20:25 +0800

    Update small-project.sh

--- kodekloud-laboratories/small-project.sh
+++ kodekloud-laboratories/small-project.sh
@@ -14,3 +14,10 @@ Adding & at the end will move the process to run in background. When you execute
 sudo sed -i 's/0.0.0.0/127.0.0.1/g;s/8080/5000/g' /opt/simple-webapp-flask/app.py
 
 # step 2
+cat /opt/simple-webapp-flask/app.py
+
+# step 3 -- run in the background
+nohup python app.py &
+
+# step 4 -- verify its running
+curl http://127.0.0.1:5000
