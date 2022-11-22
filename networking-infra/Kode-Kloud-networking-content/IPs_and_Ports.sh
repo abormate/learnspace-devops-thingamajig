4988f32fdc94395525160b55f241004d1aa7f63f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-22 10:50:51 +0800

    Update IPs_and_Ports.sh

--- networking-infra/Kode-Kloud-networking-content/IPs_and_Ports.sh
+++ networking-infra/Kode-Kloud-networking-content/IPs_and_Ports.sh
@@ -1,2 +1,5 @@
 # linux comm shows the IP addresses and details of connected interfaces in the network
 ip addr show 
+
+# Ports are comms end-point that can have 65,535 ports for each IP address. And programs can listen to a particular port -- for the example below, it is any IP address for the port 8080
+0.0.0.0:8080
