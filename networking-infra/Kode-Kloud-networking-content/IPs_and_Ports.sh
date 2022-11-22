d6eb533738513e939576d1dbaa7c2f726c1c7848
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-22 10:58:10 +0800

    Update IPs_and_Ports.sh

--- networking-infra/Kode-Kloud-networking-content/IPs_and_Ports.sh
+++ networking-infra/Kode-Kloud-networking-content/IPs_and_Ports.sh
@@ -3,3 +3,6 @@ ip addr show
 
 # Ports are comms end-point that can have 65,535 ports for each IP address. And programs can listen to a particular port -- for the example below, it is any IP address for the port 8080
 0.0.0.0:8080
+
+# loopback mirror interface IP (or localhost)
+127.0.0.1
