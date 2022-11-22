1a00281af9daf11f1218644a7be33f7b7b27e1f5
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-22 11:07:18 +0800

    Create project-apache-tomcat.sh

---
+++ kodekloud-laboratories/apache-tomcat-laboratory/project-apache-tomcat.sh
@@ -0,0 +1,6 @@
+: '
+Task -- Run apache tomcat on all IP interfaces and port 9090 on host01 server.
+
+We already downloaded apache tomcat under /opt/apache-tomcat-8.5.53 directory. Modify necessary files and start tomcat.
+'
+
