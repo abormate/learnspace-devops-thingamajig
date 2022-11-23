0f3e22341cc63a63cf422370356d7e6cea5fba39
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-23 10:42:58 +0800

    Create install-guide-for-mongodb.sh

---
+++ kodekloud-laboratories/database-stuff/mongo-db/install-guide-for-mongodb.sh
@@ -0,0 +1,6 @@
+# configure package management system for yum install
+# create a "/etc/yum.repos.d/mongodb-org-4.2.repo" (example) file so that you can install MongoDB directly with yum install
+
+yum install mongodb-org -y
+
+
