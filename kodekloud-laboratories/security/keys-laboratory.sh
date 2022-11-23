7a032b28b52e149f69c6158176863cd7a8b3ad70
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-23 14:32:11 +0800

    Update keys-laboratory.sh

--- kodekloud-laboratories/security/keys-laboratory.sh
+++ kodekloud-laboratories/security/keys-laboratory.sh
@@ -16,5 +16,16 @@ ssh -i <directory path for key file> <user>@<ip addr or hosts name>
 # example for above
 ssh -i /home/linux-user/.ssh/mykey linux2-user@127.7.8.90
 
+# generate a CSR (certificate signing request) ----------------------------
+: '
+On app01 create a CSR (certificate signing request) /etc/httpd/csr/app01.csr (key name should be app01.key)
+'
 
+# step 1
+cd /etc/httpd/csr
 
+# step 2 -- generate CSR
+sudo openssl req -new -newkey rsa:2048 -nodes -keyout app01.key -out app01.csr
+
+# step 3 -- verify entries to create CSR
+open ssl req -noout -text -in app01.csr
