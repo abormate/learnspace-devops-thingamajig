8c13482a901c400458a2c242cb595a5c4cdc31e8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-23 14:26:46 +0800

    Update keys-laboratory.sh

--- kodekloud-laboratories/security/keys-laboratory.sh
+++ kodekloud-laboratories/security/keys-laboratory.sh
@@ -10,3 +10,11 @@ scp <directory path of file> <username@ip addr or hosts path>:<destination direc
 # example of above 
 scp /home/linux_user/.ssh/mykey.pub another_linux@app01:/home/another_linux/.ssh/authorized_keys
 
+# ssh into a server if you have the keys
+ssh -i <directory path for key file> <user>@<ip addr or hosts name>
+
+# example for above
+ssh -i /home/linux-user/.ssh/mykey linux2-user@127.7.8.90
+
+
+
