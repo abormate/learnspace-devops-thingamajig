afc4503e24709e6878a76942d794d155e91f747e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-26 12:06:40 +0800

    Create sample-laboratory-create-and-launch-rocket.sh

---
+++ kodekloud-laboratories/shell-scripting/sample-laboratory-create-and-launch-rocket.sh
@@ -0,0 +1,11 @@
+mkdir lunar-mission
+rocket-add lunar-mission
+rocket-start-power lunar-mission
+rocket-internal-power lunar-mission
+rocket-start-sequence lunar-mission
+rocket-start-engine lunar-mission
+rocket-lift-off lunar-mission
+rocket status lunar-mission
+
+# don't forget to modify the file permissions for this to become executable when run
+# chmod +x
