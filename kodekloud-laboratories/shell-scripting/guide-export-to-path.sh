583e998d6de15fe7d466e7afda6c5a075035bd4b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-26 11:44:12 +0800

    Update guide-export-to-path.sh

--- kodekloud-laboratories/shell-scripting/guide-export-to-path.sh
+++ kodekloud-laboratories/shell-scripting/guide-export-to-path.sh
@@ -2,3 +2,12 @@
 # example for "create-and-launch-rocket.sh" -- sh file doesn't run right away when you type the name of the file, without the "sh" file extension -- much like any generic command in Linux 
 # shell script below typed as a command below doesn't run -- will throw an error
 create-and-launch-rocket
+
+# look for the directories defined in the system PATH
+# to do so, run the following command 
+echo $PATH
+
+# echo command above, outputs the PATH directories 
+# which could look like this as an output --> /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+
