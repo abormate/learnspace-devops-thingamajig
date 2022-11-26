cf571b7d96a18927e4e02704801690d5f19281f7
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-26 11:04:26 +0800

    Create project-rocket-laboratory-guide.sh

---
+++ kodekloud-laboratories/shell-scripting/project-rocket-laboratory-guide.sh
@@ -0,0 +1,8 @@
+
+: '
+Let's create a new directory for our new mission. Every mission requires a new directory created by the mission name. Create a new directory at /home/bob named lunar-mission.
+
+Note that all future rocket commands must be run from the parent directory of this mission directory. That is /home/bob in your case
+'
+
+mkdir lunar-mission
