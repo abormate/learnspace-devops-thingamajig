cd7920a57320c9b3ebba7a58d15919ab50ecd5a0
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-26 11:41:50 +0800

    Create guide-export-to-path.sh

---
+++ kodekloud-laboratories/shell-scripting/guide-export-to-path.sh
@@ -0,0 +1,4 @@
+# If you place linux commands in series and sequence inside a shell file (*.sh file). The commands will run in order one after another. 
+# example for "create-and-launch-rocket.sh" -- sh file doesn't run right away when you type the name of the file, without the "sh" file extension -- much like any generic command in Linux 
+# shell script below typed as a command below doesn't run -- will throw an error
+create-and-launch-rocket
