43828ff458d194601036e902a4d2d3768bdf9d7c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-26 11:05:20 +0800

    Update project-rocket-laboratory-guide.sh

--- kodekloud-laboratories/shell-scripting/project-rocket-laboratory-guide.sh
+++ kodekloud-laboratories/shell-scripting/project-rocket-laboratory-guide.sh
@@ -1,7 +1,11 @@
+:'
+Rocket commands have been configured on your system. Let's test it out. Run the rocket-ls command to list all rockets.
+'
 
-: '
+rocket-ls
+
+:'
 Let's create a new directory for our new mission. Every mission requires a new directory created by the mission name. Create a new directory at /home/bob named lunar-mission.
-
 Note that all future rocket commands must be run from the parent directory of this mission directory. That is /home/bob in your case
 '
 
