c72e6d4cb2a765d3605f409d59e6cdef926920f0
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-28 10:50:40 +0800

    Create print-uptime-linux.sh

---
+++ kodekloud-laboratories/shell-scripting/print-uptime-linux.sh
@@ -0,0 +1,5 @@
+#print uptime through variable --> note that variables cannot use dashes, only underscores and is only alphanumeric
+up_time=$(uptime)
+
+echo "The uptime of the system is $up_time"
+
