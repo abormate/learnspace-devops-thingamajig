a1ec2fb340591c6a9a81d2a64629bfdb1acc4ab6
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-28 10:51:32 +0800

    Update print-uptime-var-linux.sh

--- kodekloud-laboratories/shell-scripting/print-uptime-var-linux.sh
+++ kodekloud-laboratories/shell-scripting/print-uptime-var-linux.sh
@@ -1,4 +1,4 @@
-#print uptime through variable --> note that variables cannot use dashes, only underscores and is only alphanumeric
+#print uptime through variable in Ubuntu at least --> note that variables cannot use dashes, only underscores and is only alphanumeric
 up_time=$(uptime)
 
 echo "The uptime of the system is $up_time"
