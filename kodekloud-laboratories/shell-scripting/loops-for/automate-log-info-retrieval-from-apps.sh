75fa04c6edff987e388911ea2fbf0f7b6236450b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-29 09:43:23 +0800

    Update automate-log-info-retrieval-from-apps.sh

--- kodekloud-laboratories/shell-scripting/loops-for/automate-log-info-retrieval-from-apps.sh
+++ kodekloud-laboratories/shell-scripting/loops-for/automate-log-info-retrieval-from-apps.sh
@@ -11,3 +11,15 @@ Finance 10 20 50
 
 Marketing 20 10 30
 '
+
+echo -e " Log name   \t      GET      \t      POST    \t   DELETE "
+echo -e "------------------------------------------------------------"
+
+for app in $(cat /tmp/assets/apps.txt)
+do
+  get_requests=$(cat /var/log/apps/${app}_app.log | grep "GET" | wc -l)
+  post_requests=$(cat /var/log/apps/${app}_app.log | grep "POST" | wc -l)
+  delete_requests=$(cat /var/log/apps/${app}_app.log | grep "DELETE" | wc -l)
+  echo -e " ${app}    \t ${get_requests}    \t    ${post_requests}   \t   ${delete_requests}"
+
+done
