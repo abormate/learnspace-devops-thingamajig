02145dfa21b288f0d5014a148eac6fd869af45d8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-29 09:48:52 +0800

    Update rename-batch-files.sh

--- kodekloud-laboratories/shell-scripting/loops-for/rename-batch-files.sh
+++ kodekloud-laboratories/shell-scripting/loops-for/rename-batch-files.sh
@@ -10,3 +10,11 @@ Tip: Use mv to rename a file.
 To replace jpeg to jpg in a filename use echo user1.jpeg | sed 's/jpeg/jpg/g'
 '
 
+for file in $(ls images)
+do
+        if [[ $file = *.jpeg ]]
+                then
+                new_name=$(echo $file| sed 's/jpeg/jpg/g')
+                mv images/$file images/$new_name
+        fi
+done
