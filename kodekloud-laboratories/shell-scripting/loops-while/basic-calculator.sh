8a023b5053f51f39ef2ca746b681edd2f5f4a77e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-29 10:34:10 +0800

    Create basic-calculator.sh

---
+++ kodekloud-laboratories/shell-scripting/loops-while/basic-calculator.sh
@@ -0,0 +1,15 @@
+:'
+Let us now build a menu driven calculator program. Develop a script /home/bob/calculator.sh that when run:
+
+Shows a menu driven program with the following options:
+
+Add
+Subtract
+Multiply
+Divide
+Quit
+Depending on the input the program must ask for 2 numbers - Number1 and Number2 and then print the result in the form Answer=6.
+
+The program must then show the menu again until user selects option 5 to quit.
+Note :- Script should be in an executable format, run command chmod +x to make it executable.
+'
