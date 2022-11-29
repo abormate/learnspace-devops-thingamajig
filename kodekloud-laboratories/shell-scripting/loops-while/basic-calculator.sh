06659d195c5cd579ab2af0b08d0b9ad2fb905ecd
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-29 10:35:04 +0800

    Update basic-calculator.sh

--- kodekloud-laboratories/shell-scripting/loops-while/basic-calculator.sh
+++ kodekloud-laboratories/shell-scripting/loops-while/basic-calculator.sh
@@ -13,3 +13,40 @@ Depending on the input the program must ask for 2 numbers - Number1 and Number2 
 The program must then show the menu again until user selects option 5 to quit.
 Note :- Script should be in an executable format, run command chmod +x to make it executable.
 '
+
+while true
+do
+  echo "1. Add"
+  echo "2. Subtract"
+  echo "3. Multiply"
+  echo "4. Divide"
+  echo "5. Quit"
+
+  read -p "Enter your choice: " choice
+
+  if [ $choice -eq 1 ]
+  then
+        read -p "Enter Number1: " number1
+        read -p "Enter Number2: " number2
+        echo Answer=$(( $number1 + $number2 ))
+  elif [ $choice -eq 2 ]
+  then
+        read -p "Enter Number1: " number1
+        read -p "Enter Number2: " number2
+        echo Answer=$(( $number1 - $number2 ))
+  elif [ $choice -eq 3 ]
+  then
+        read -p "Enter Number1: " number1
+        read -p "Enter Number2: " number2
+        echo Answer=$(( $number1 * $number2 ))
+  elif [ $choice -eq 4 ]
+  then
+        read -p "Enter Number1: " number1
+        read -p "Enter Number2: " number2
+        echo Answer=$(( $number1 / $number2 ))
+  elif [ $choice -eq 5 ]
+  then
+    break
+  fi
+
+done
