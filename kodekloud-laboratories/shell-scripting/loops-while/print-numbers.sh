9346615c651bb25dab3b7c7c16b1b3afedec468b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-29 10:30:13 +0800

    Create print-numbers.sh

---
+++ kodekloud-laboratories/shell-scripting/loops-while/print-numbers.sh
@@ -0,0 +1,6 @@
+:'
+A script called print-numbers.sh is created in the home directory. There is a syntax error in the script. Identify it and fix it so that it works
+
+Execute the script with a number as an argument - ./print-numbers.sh 10. Do not make any other changes to the script other than fixing the error.
+Note :- Run the command chmod +x to make it script executable .
+'
