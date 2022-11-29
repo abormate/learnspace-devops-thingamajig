122c8a44e8af8c472c9d643dcaee08f5a6794de2
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-11-29 10:21:59 +0800

    Update laboratory-project-1.sh

--- kodekloud-laboratories/shell-scripting/loops-while/laboratory-project-1.sh
+++ kodekloud-laboratories/shell-scripting/loops-while/laboratory-project-1.sh
@@ -1,1 +1,5 @@
+:'
+Let's update our script to take into consideration the time a rocket spends in a launching state.
 
+The script is available at the path /home/bob/create-and-launch-rocket. Add a while loop before the if conditional statements that checks the status of rocket to wait until the rocket is in a launching state
+'
