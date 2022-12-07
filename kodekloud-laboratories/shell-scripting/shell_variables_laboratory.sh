4ba33ca28f14f713d506266ed84a976b04453a48
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-12-07 18:14:15 +0800

    Update shell_variables_laboratory.sh

--- kodekloud-laboratories/shell-scripting/shell_variables_laboratory.sh
+++ kodekloud-laboratories/shell-scripting/shell_variables_laboratory.sh
@@ -1,8 +1,16 @@
-:'
+# 1st lab task
+:' Instructions
 Another script by the name print-uptime.sh is placed in your home directory at /home/bob.
 It must print the uptime of the system as expected. However it's not working'. Please inspect and fix the problem with the script.
 '
 
 up_time=$(uptime)
-  
 echo "The uptime of the system is $up_time"
+
+# shell script above prints the Linux server uptime
+
+# 2nd lab task
+:' Instructions
+Another script by the name backup-file.sh is placed in your home directory at /home/bob.
+This script creates a backup of a file by creating a copy of the same file and apending _bkp to it's name. However it's not working'. Please inspect and fix the problem with the script.
+'
