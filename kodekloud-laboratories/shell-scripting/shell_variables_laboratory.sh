07fc6b3cbfa7730a39cfafd28a11dd26b980a6e3
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-12-07 18:12:20 +0800

    Update shell_variables_laboratory.sh

--- kodekloud-laboratories/shell-scripting/shell_variables_laboratory.sh
+++ kodekloud-laboratories/shell-scripting/shell_variables_laboratory.sh
@@ -1,4 +1,7 @@
-#
+:'
+Another script by the name print-uptime.sh is placed in your home directory at /home/bob.
+It must print the uptime of the system as expected. However it's not working'. Please inspect and fix the problem with the script.
+'
 
 up_time=$(uptime)
   
