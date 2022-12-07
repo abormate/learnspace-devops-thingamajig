3815b6d4104db058d88f5ac9b7dc18ef986fe22f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-12-07 17:52:21 +0800

    Update shell-variables-guide.sh

--- kodekloud-laboratories/shell-scripting/shell-variables-guide.sh
+++ kodekloud-laboratories/shell-scripting/shell-variables-guide.sh
@@ -1,2 +1,11 @@
-# variables can only be defined with alphanumeric characters and underscores and no with symbols and dashes
+# variables can only be defined with alphanumeric characters and underscores and no with symbols and dashes. But their assignment value can hold dashes.
 # variable definitions are case sensitive. For example, "MISSION_NAME" is not equal to "mission_name"
+
+# example variable in action
+mission_name=mars-mission
+mkdir $mission_name
+
+rocket-start-power $mission_name
+rocket-internal-power $mission_name
+rocket-crew-ready $mission_name
+rocket-start-sequence $mission_name
