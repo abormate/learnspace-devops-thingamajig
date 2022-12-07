2600c4b7dcf1fd63d3355efc22e79338bc24cdfa
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-12-07 17:50:18 +0800

    Update shell-variables-guide.sh

--- kodekloud-laboratories/shell-scripting/shell-variables-guide.sh
+++ kodekloud-laboratories/shell-scripting/shell-variables-guide.sh
@@ -1,1 +1,2 @@
 # variables can only be defined with alphanumeric characters and underscores and no with symbols and dashes
+# variable definitions are case sensitive. For example, "MISSION_NAME" is not equal to "mission_name"
