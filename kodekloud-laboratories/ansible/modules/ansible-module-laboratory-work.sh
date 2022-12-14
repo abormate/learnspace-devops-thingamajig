32a6ea46b5f0d5a1dc2b62f0a2009c0e5d957841
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-12-14 19:15:18 +0800

    Update ansible-module-laboratory-work.sh

--- kodekloud-laboratories/ansible/modules/ansible-module-laboratory-work.sh
+++ kodekloud-laboratories/ansible/modules/ansible-module-laboratory-work.sh
@@ -3,3 +3,14 @@ Update the playbook with a play to Execute a script on all web server nodes. The
 Use the Script module
 '
 
+# start ansible shell script from this line
+-
+
+    name: 'Execute a script on all web server nodes'
+    hosts: web_nodes
+    tasks:
+        -
+            name: 'Execute a script on all web server nodes'
+            script: /tmp/install_script.sh
+            
+# end ansible script on this line for question/task 1
