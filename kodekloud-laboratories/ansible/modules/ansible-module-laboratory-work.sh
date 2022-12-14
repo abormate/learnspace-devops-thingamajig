c7ebd96a87ccd26f68c4e8ac28748bd69d9c2151
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-12-14 19:40:49 +0800

    Update ansible-module-laboratory-work.sh

--- kodekloud-laboratories/ansible/modules/ansible-module-laboratory-work.sh
+++ kodekloud-laboratories/ansible/modules/ansible-module-laboratory-work.sh
@@ -42,4 +42,37 @@ Use the Service module
 #
 #
 #
+# task 3
+:'
+Update the playbook to add a new task in the beginning to add an entry into /etc/resolv.conf file for hosts. The line to be added is nameserver 10.1.250.10
+
+Note: The new task must be executed first, so place it accordingly.
+
+Use the Lineinfile module
+'
+#
+#
+# script shell on the following for ansible
+
+-
+    name: 'Execute a script on all web server nodes'
+    hosts: web_nodes
+    tasks:
+        -
+            name: 'Update entry into /etc/resolv.conf'
+            lineinfile:
+                path: /etc/resolv.conf
+                line: 'nameserver 10.1.250.10'
+        -
+            name: 'Execute a script'
+            script: /tmp/install_script.sh
+        -
+            name: 'Start httpd service'
+            service:
+                name: httpd
+                state: present
+
+# end of shell script on this line
+#
+#
 #
