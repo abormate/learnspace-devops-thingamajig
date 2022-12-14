8fda1c1372964aa9e78985e8c18fafe72148cb61
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-12-14 18:39:35 +0800

    Update and rename guide.sh to guide-to-ansible-modules.sh

---
+++ kodekloud-laboratories/ansible/modules/guide-to-ansible-modules.sh
@@ -0,0 +1,7 @@
+# Ansible are categorized into various groups based on their functionality.
+
+# module category -- System --> actions to performed on a system level
+# user, group, hostname, iptables, lvg, lvol, make, mount, ping, timezone, systemd, service
+
+# module category -- Commands --> used to execute commands or scripts on a host
+# command, expect, raw, script, shell
--- kodekloud-laboratories/ansible/modules/guide.sh
+++
@@ -1,4 +0,0 @@
-# Ansible are categorized into various groups based on their functionality.
-
-# category -- System
-# user, group, hostname, iptables, lvg, lvol, make, mount, ping, timezone, systemd, service
