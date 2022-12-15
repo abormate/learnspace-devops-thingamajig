eb1d5a3583a9af97331da9dc8e511dd5fcfca31f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-12-15 13:36:01 +0800

    Update guide-to-ansible-variables.sh

--- kodekloud-laboratories/ansible/variables/guide-to-ansible-variables.sh
+++ kodekloud-laboratories/ansible/variables/guide-to-ansible-variables.sh
@@ -1,7 +1,9 @@
-# Stores information that varies across each host
+# Variables store information that varies across each host
 #
 #
 # Example: Variables in an inventory file example from previous lectures. 
 # For the following, "ansible_host", "ansible_connection", and "ansible_ssh_pass" are variables.
 
-web1 ansible_host=server.1company.com   ansible_connection=ssh  ansible_ssh_pass=P@ssw
+web1 ansible_host=server1.company.com   ansible_connection=ssh    ansible_ssh_pass=P@ssw
+db   ansible_host=server2.company.com   ansible_connection=ssh    ansible_ssh_pass=P@ssw
+
