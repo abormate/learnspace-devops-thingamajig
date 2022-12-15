14bb2cb44184ebb06d213ee92fe3fb09e0ec8ff3
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-12-15 13:39:37 +0800

    Update guide-to-ansible-variables.sh

--- kodekloud-laboratories/ansible/variables/guide-to-ansible-variables.sh
+++ kodekloud-laboratories/ansible/variables/guide-to-ansible-variables.sh
@@ -7,3 +7,17 @@
 web1  ansible_host=server1.company.com    ansible_connection=ssh    ansible_ssh_pass=P@ssw
 db    ansible_host=server2.company.com    ansible_connection=winrm  ansible_ssh_pass=P@ssw
 web2  ansible_host=server3.company.com    ansible_connection=ssh    ansible_ssh_pass=P@ssw
+
+#
+# Another example, could be done on a Playbook
+# Whereas the module for "vars" define the variable in the following playbook
+
+-
+  name: Add DNS server to resolv.conf
+  hosts: localhost
+  vars:
+    dns_server: 10.1.250.10
+  tasks:
+    - lineinfile
+      path: /etc/resolv.conf
+      line: 'nameserver 10.1.250.10'
