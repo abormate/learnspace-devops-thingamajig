02066ae174399ee26abd5171267060aea1c058c1
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2022-12-30 16:03:07 +0800

    Update install-jenkins-on-ubuntu.sh

--- kodekloud-laboratories/jenkins/install-jenkins-on-ubuntu.sh
+++ kodekloud-laboratories/jenkins/install-jenkins-on-ubuntu.sh
@@ -1,5 +1,19 @@
 #
 # Once you're in an Ubuntu machine
 # Update the machine first
+#
 
 sudo apt update -y
+
+#
+# Install first dependency which is Java dev kit
+#
+#
+
+sudo apt install openjdk-11-jdk
+
+#
+#
+#
+#
+
