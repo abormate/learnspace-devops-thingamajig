b25b60c9eeb8e1bcd35b87d76db769ed99386a35
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-01-02 16:41:11 +0800

    Create installation-lab-jenkins.sh

---
+++ kodekloud-laboratories/jenkins/installation-lab-jenkins.sh
@@ -0,0 +1,9 @@
+#
+#
+#
+root@jenkins-server:~# id mike
+uid=1001(mike) gid=1002(mike) groups=1002(mike),27(sudo)
+root@jenkins-server:~# 
+
+root@jenkins-server:~# su - mike 
+mike@jenkins-server:~$
