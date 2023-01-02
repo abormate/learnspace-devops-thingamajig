25a0cbb5f079048d497b364fa14c6057855c0041
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-01-02 16:43:24 +0800

    Update installation-lab-jenkins.sh

--- kodekloud-laboratories/jenkins/installation-lab-jenkins.sh
+++ kodekloud-laboratories/jenkins/installation-lab-jenkins.sh
@@ -7,3 +7,35 @@ root@jenkins-server:~#
 
 root@jenkins-server:~# su - mike 
 mike@jenkins-server:~$
+#
+#
+#
+#
+
+curl -Lv http://localhost:8085/login 2>&1 | grep -i 'x-ssh-endpoint'
+
+Which port does the Jenkins SSH service use?
+
+
+8888
+
+
+22
+
+
+8022
+
+
+8085
+
+menu
+open_in_new
+wb_sunny
+settings_backup_restore
+stop
+fullscreen
+settings_input_hdmi Jenkins
+language Jenkins CLI Docs
+Terminal 1
+
+
