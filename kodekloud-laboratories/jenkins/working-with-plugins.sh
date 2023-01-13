1a447cb53b12d43804dfa0e05f84bc107ae172f6
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-01-13 12:18:10 +0800

    Update working-with-plugins.sh

--- kodekloud-laboratories/jenkins/working-with-plugins.sh
+++ kodekloud-laboratories/jenkins/working-with-plugins.sh
@@ -1,7 +1,7 @@
+---
+
 # when you need to restart after implementing or installing a Jenkins plugin. Additionally, you may need a restart after doing some config changes
 sudo systemctl restart jenkins
 
 # and after executing do a status check
 systemctl status jenkins
-
-----
