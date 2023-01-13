875531d6feecd59cf551436d177ef16d5dd99d08
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-01-13 12:27:20 +0800

    Update laboratory-plugins.sh

--- kodekloud-laboratories/jenkins/laboratory-plugins.sh
+++ kodekloud-laboratories/jenkins/laboratory-plugins.sh
@@ -1,2 +1,5 @@
 # Jenkins CLI to interact with install service from shell -- in this example, one that connects to localhost with credentials
 java -jar jenkins-cli.jar -s http://localhost:8085 -auth 'admin:Adm!n321'
+
+# following command lists all installed plugins for Jenkins
+list-plugins
