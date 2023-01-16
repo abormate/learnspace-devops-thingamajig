f2bda581db12b5a77966ca6ac2551b5591605d6e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-01-17 06:25:00 +0800

    Update laboratory-plugins.sh

--- kodekloud-laboratories/jenkins/laboratory-plugins.sh
+++ kodekloud-laboratories/jenkins/laboratory-plugins.sh
@@ -5,5 +5,5 @@ java -jar jenkins-cli.jar -s http://localhost:8085 -auth 'admin:Adm!n321'
 java -jar jenkins-cli.jar -s http://localhost:8085 -auth 'admin:Adm!n321' list-plugins
 
 
----
+------
 -------
