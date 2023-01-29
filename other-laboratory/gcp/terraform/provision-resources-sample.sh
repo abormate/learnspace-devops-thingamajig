cefe17e6d936f6fb2b05f9b5fc4992ceaf6bf128
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-01-29 12:12:59 +0800

    Update provision-resources-sample.sh

--- other-laboratory/gcp/terraform/provision-resources-sample.sh
+++ other-laboratory/gcp/terraform/provision-resources-sample.sh
@@ -8,11 +8,26 @@ terraform {
 
 provider "google" {
     version = "3.5.0"
-    project = "qwiklabs-gcp-03-9aa8b3de2bc8"
+    project = ""
     region = "us-central1"
     zone = "us-central1-c"
 }
 
 resource "google_compute_network" "vpc_network" {
     name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
 }
