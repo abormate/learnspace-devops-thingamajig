9519e69e6ebf93b2b847c79a6d5763b826c6ee8d
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-01-30 15:54:19 +0800

    Update workflow.sh

--- other-laboratory/google-cloud-platform/manage-deployments-kubernetes-engine/workflow.sh
+++ other-laboratory/google-cloud-platform/manage-deployments-kubernetes-engine/workflow.sh
@@ -1,1 +1,6 @@
-
+:'
+Dev Ops practices will regularly make use of multiple deployments to manage application deployment scenarios 
+such as "Continuous deployment", "Blue-Green deployments", "Canary deployments" and more. 
+This is to provide practice in scaling and managing containers so you can accomplish these common scenarios where multiple heterogeneous 
+deployments are being used.
+'
