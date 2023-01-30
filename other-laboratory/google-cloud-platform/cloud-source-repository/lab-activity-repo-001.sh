112bd612b9653b5f3b19933ad0df014566f971a5
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-01-30 10:15:51 +0800

    Update lab-activity-repo-001.sh

--- other-laboratory/google-cloud-platform/cloud-source-repository/lab-activity-repo-001.sh
+++ other-laboratory/google-cloud-platform/cloud-source-repository/lab-activity-repo-001.sh
@@ -3,3 +3,7 @@ gcloud auth list
 
 # List project ID active on session current
 gcloud config list project
+
+# Create new cloud source repo with name "REPO_DEMO"
+gcloud source repos create REPO_DEMO
+
