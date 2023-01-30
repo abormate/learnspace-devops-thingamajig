5ecb6843ff35928ab42ea70be43408365900bed9
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-01-30 15:55:35 +0800

    Update workflow.sh

--- other-laboratory/google-cloud-platform/manage-deployments-kubernetes-engine/workflow.sh
+++ other-laboratory/google-cloud-platform/manage-deployments-kubernetes-engine/workflow.sh
@@ -4,3 +4,16 @@ such as "Continuous deployment", "Blue-Green deployments", "Canary deployments" 
 This is to provide practice in scaling and managing containers so you can accomplish these common scenarios where multiple heterogeneous 
 deployments are being used.
 '
+#
+#
+#
+#
+
+:'
+Heterogeneous deployments typically involve connecting two or more distinct infrastructure environments or regions to address 
+a specific technical or operational need. Heterogeneous deployments are called "hybrid", "multi-cloud", or "public-private", 
+depending upon the specifics of the deployment.
+
+For the purposes of this lab, heterogeneous deployments include those that span regions within a single cloud environment, 
+multiple public cloud environments (multi-cloud), or a combination of on-premises and public cloud environments (hybrid or public-private).
+'
