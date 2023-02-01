74073a8f5d3ba991c4a5c3098478fa953902c09a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-01 20:14:10 +0800

    Update kubernetes-laboratory-01.sh

--- kodekloud-laboratories/docker-kubernetes/kubernetes-laboratory-01.sh
+++ kodekloud-laboratories/docker-kubernetes/kubernetes-laboratory-01.sh
@@ -4,4 +4,5 @@ kubectl get nodes
 # What is the version of Kubernetes running on an instance?
 kubectl version --short
 
+# What is the Linux distro and version from which Kubernetes instance is running from?
 
