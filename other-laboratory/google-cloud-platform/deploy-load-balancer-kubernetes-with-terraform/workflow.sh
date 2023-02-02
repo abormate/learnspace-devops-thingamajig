7113d2edd00c3a8dc06cd33117118dbda3b3b46c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-02 13:07:42 +0800

    Update workflow.sh

--- other-laboratory/google-cloud-platform/deploy-load-balancer-kubernetes-with-terraform/workflow.sh
+++ other-laboratory/google-cloud-platform/deploy-load-balancer-kubernetes-with-terraform/workflow.sh
@@ -15,3 +15,15 @@ terraform init
 
 # Apply terraform plan made from declarative main manifest and variables
 terraform apply
+
+# Sample output from Terraform apply command 
+:'
+Apply complete! Resources: 7 added, 0 changed, 0 destroyed.
+Outputs:
+cluster_name = tf-gke-k8s
+cluster_region = us-west1
+cluster_zone = us-west1-b
+load-balancer-ip = 35.233.177.223
+network = https://www.googleapis.com/compute/beta/projects/qwiklabs-gcp-5438ad3a5e852e4a/global/networks/tf-gke-k8s
+subnetwork_name = tf-gke-k8s
+'
