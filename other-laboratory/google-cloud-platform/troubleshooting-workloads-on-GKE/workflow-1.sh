7777d4ba5ab2a58138fe4188da59e6ea3cf9cf52
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-02 15:27:21 +0800

    Update workflow-1.sh

--- other-laboratory/google-cloud-platform/troubleshooting-workloads-on-GKE/workflow-1.sh
+++ other-laboratory/google-cloud-platform/troubleshooting-workloads-on-GKE/workflow-1.sh
@@ -1,1 +1,12 @@
+:'
+Site Reliability Engineers (SRE) have a broad set of responsibilities, and managing incidents is a critical part of their role. 
+You will learn how to take advantage of the integrated capabilities of Google Cloud's operations suite that includes logging, 
+monitoring, and rich, out-of-the-box dashboards.
 
+The troubleshooting process is an “iterative” approach where SREs form a hypothesis about the potential root cause of an incident, 
+then filter, search, and navigate through large volumes of telemetry data collected from their systems to validate or invalidate their hypothesis. 
+If a hypothesis is invalid, SREs will form another hypothesis and perform another iteration until they can isolate a root cause.
+'
+
+# Activate Cloud shell -- the following shell code lists the active account name
+gcloud auth list
