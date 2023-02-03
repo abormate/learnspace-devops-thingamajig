a810efdb8bda5a0fcfffea7e8dc98288d3f02087
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-03 19:32:24 +0800

    Update workflow-guide-01.sh

--- other-laboratory/google-cloud-platform/continuous-delivery-jenkins-with-kubernetes/workflow-guide-01.sh
+++ other-laboratory/google-cloud-platform/continuous-delivery-jenkins-with-kubernetes/workflow-guide-01.sh
@@ -1,3 +1,13 @@
 #
+# Download source code -- Task 1
+#
+gcloud config set compute/zone
+
+# Copy the laboratory's sample code
+gsutil cp gs://spls/gsp051/continuous-deployment-on-kubernetes.zip .
+
+unzip continuous-deployment-on-kubernetes.zip
+
+#
 #
 #
