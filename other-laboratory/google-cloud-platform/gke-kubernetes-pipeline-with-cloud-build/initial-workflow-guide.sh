a779fa7cd192cc270e910f7ec6f6956b2d0165da
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-19 04:26:33 +0800

    Update initial-workflow-guide.sh

--- other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/initial-workflow-guide.sh
+++ other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/initial-workflow-guide.sh
@@ -3,8 +3,27 @@
 In this lab, you create a CI/CD pipeline that automatically builds a container image from committed code, stores the image in Artifact Registry, 
 updates a Kubernetes manifest in a Git repository, and deploys the application to Google Kubernetes Engine using that manifest.
 '
-
 #
+# You'll create 2 repositories
+#
+# -- app repository: contains the source code of the application itself
+# -- env repository: contains the manifests for the Kubernetes Deployment
 #
 
+:'
+When you push a change to the app repository, the Cloud Build pipeline runs tests, builds a container image, and pushes it to Artifact Registry. 
+After pushing the image, Cloud Build updates the Deployment manifest and pushes it to the env repository. This triggers another Cloud Build pipeline 
+that applies the manifest to the GKE cluster and, if successful, stores the manifest in another branch of the env repository.
+
+The app and env repositories are kept separate because they have different lifecycles and uses. The main users of the app repository are 
+actual humans and this repository is dedicated to a specific application. The main users of the env repository are automated systems 
+(such as Cloud Build), and this repository might be shared by several applications. The env repository can have several branches that each map to 
+a specific environment (you only use production in this lab) and reference a specific container image, whereas the app repository does not.
+
+When you finish this lab, you have a system where you can easily:
+
+-- Distinguish between failed and successful deployments by looking at the Cloud Build history.
+-- Access the manifest currently used by looking at the production branch of the env repository.
+-- Rollback to any previous version by re-executing the corresponding Cloud Build build.
+'
 
