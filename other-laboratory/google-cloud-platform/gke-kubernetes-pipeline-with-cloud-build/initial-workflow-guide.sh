3ee895f5d1702c0db657d6820a101f8b214ff2b9
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-19 04:18:37 +0800

    Update and rename workflow-guide.sh to initial-workflow-guide.sh

---
+++ other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/initial-workflow-guide.sh
@@ -0,0 +1,10 @@
+# Description process 
+:'
+In this lab, you create a CI/CD pipeline that automatically builds a container image from committed code, stores the image in Artifact Registry, 
+updates a Kubernetes manifest in a Git repository, and deploys the application to Google Kubernetes Engine using that manifest.
+'
+
+#
+#
+
+
