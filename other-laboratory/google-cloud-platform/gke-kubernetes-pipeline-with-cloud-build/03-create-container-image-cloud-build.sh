af4fc5876e11667c219dda5046e6f1bd211f4dc8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-20 05:17:52 +0800

    Update 03-create-container-image-cloud-build.sh

--- other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/03-create-container-image-cloud-build.sh
+++ other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/03-create-container-image-cloud-build.sh
@@ -1,1 +1,21 @@
+# -------------------------------------------- #
+#
+# Task 3 - Create container image with Cloud Build
+#
+# -------------------------------------------- #
 
+#
+# Code cloned already contains following Dockerfile
+#
+
+FROM python:3.7-slim
+RUN pip install flask
+WORKDIR /app
+COPY app.py /app/app.py
+ENTRYPOINT ["python"]
+CMD ["/app/app.py"]
+
+#
+#
+#
+#
