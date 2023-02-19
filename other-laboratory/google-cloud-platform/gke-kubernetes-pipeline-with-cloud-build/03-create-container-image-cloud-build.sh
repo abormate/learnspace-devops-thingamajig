914c4d99334b52cdc8283fd3dd86938b55f57b09
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-20 05:22:28 +0800

    Update 03-create-container-image-cloud-build.sh

--- other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/03-create-container-image-cloud-build.sh
+++ other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/03-create-container-image-cloud-build.sh
@@ -16,8 +16,8 @@ ENTRYPOINT ["python"]
 CMD ["/app/app.py"]
 
 #
-#
-#
+# With this (above) Dockerfile, you can create a container image with Cloud Build and store it in Artifact Registry.
+# In Cloud shell, create build based on the latest commit with the following
 #
 
-The app and env repositories are kept separate because they have different lifecycles and uses. The main users of the app repository are actual humans and this repository is dedicated to a specific application. The main users of the env repository are automated systems (such as Cloud Build), and this repository might be shared by several applications. The env repository can have several branches that each map to a specific environment (you only use production in this lab) and reference a specific container image, whereas the app repository does not.
+cd ~/hello-cloudbuild-app
