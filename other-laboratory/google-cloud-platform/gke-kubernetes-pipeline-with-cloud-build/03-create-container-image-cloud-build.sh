4dfc01def49ff0fadc200251efc715bfcd1b9862
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-20 05:20:37 +0800

    Update 03-create-container-image-cloud-build.sh

--- other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/03-create-container-image-cloud-build.sh
+++ other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/03-create-container-image-cloud-build.sh
@@ -19,3 +19,5 @@ CMD ["/app/app.py"]
 #
 #
 #
+
+The app and env repositories are kept separate because they have different lifecycles and uses. The main users of the app repository are actual humans and this repository is dedicated to a specific application. The main users of the env repository are automated systems (such as Cloud Build), and this repository might be shared by several applications. The env repository can have several branches that each map to a specific environment (you only use production in this lab) and reference a specific container image, whereas the app repository does not.
