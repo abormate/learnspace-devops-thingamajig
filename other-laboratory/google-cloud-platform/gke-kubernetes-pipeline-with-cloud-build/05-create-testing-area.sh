6eadad984c56828b58e8fdac683ca9135df2380d
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-21 04:42:33 +0800

    Update 05-create-testing-area.sh

--- other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/05-create-testing-area.sh
+++ other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/05-create-testing-area.sh
@@ -29,4 +29,6 @@ triggering the continuous delivery pipeline.
 
 PROJECT_NUMBER="$(gcloud projects describe ${PROJECT_ID} --format='get(projectNumber)')"
 
-
+gcloud projects add-iam-policy-binding ${PROJECT_NUMBER} \
+--member=serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com \
+--role=roles/container.developer
