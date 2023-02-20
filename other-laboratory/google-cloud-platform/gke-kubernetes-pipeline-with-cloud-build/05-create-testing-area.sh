93a5916a771441db2d3b15d61196c79f809339e6
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-21 04:40:04 +0800

    Update 05-create-testing-area.sh

--- other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/05-create-testing-area.sh
+++ other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/05-create-testing-area.sh
@@ -6,6 +6,8 @@
 # --------------------------------- #
 
 #
-#
-#
-#
+:'
+Cloud Build is also used for the continuous delivery pipeline. The pipeline runs each time a commit is pushed to the candidate branch of 
+the hello-cloudbuild-env repository. The pipeline applies the new version of the manifest to the Kubernetes cluster and, if successful, 
+copies the manifest over to the production branch. This process has the following properties:
+'
