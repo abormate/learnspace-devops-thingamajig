fc19b5d26c79f82dd96f1d41e8eca7d35bfcff84
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-22 04:24:24 +0800

    Added steps for Cloud Build pipeline CI/CD

--- other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/06-review-and-complete-pipeline.sh
+++ other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/06-review-and-complete-pipeline.sh
@@ -1,1 +1,12 @@
+# ---------------------------- #
+#
+# Task 6 - Review code build pipeline
+#
+# ---------------------------- #
 
+#
+# Most of the procedure steps here could be done via the visual Console
+#
+#
+
+# Step 1 - In the console, go to: Cloud Build > Dashboard
