73ff7688e63ea7b1bdc38106c1b57476d363e47a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-22 04:27:21 +0800

    Added a 2nd step to task 6 of Cloud Build pipeline for CI/CD

--- other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/06-review-and-complete-pipeline.sh
+++ other-laboratory/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/06-review-and-complete-pipeline.sh
@@ -6,7 +6,19 @@
 
 #
 # Most of the procedure steps here could be done via the visual Console
-#
+# Not much code required for this particular task
 #
 
-# Step 1 - In the console, go to: Cloud Build > Dashboard
+#
+# Step 1 -- In the console, go to: Cloud Build > Dashboard
+#
+
+#
+# Step 2 -- Examine the logs of "hello-cloudbuild-app" trigger and follow its execution. 
+# The last step of this pipeline pushes the new manifest to the "hello-cloudbuild-env" repo,
+# which triggers the continuous delivery pipeline.
+#
+
+#
+#
+#
