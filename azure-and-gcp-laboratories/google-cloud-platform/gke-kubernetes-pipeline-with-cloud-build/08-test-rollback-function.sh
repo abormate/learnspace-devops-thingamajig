3d970a7a613b1cdc17e274aea1f709f34057c72a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-25 17:30:18 +0800

    08 - Test Rollback - DevOps - GCP

---
+++ azure-and-gcp-laboratories/google-cloud-platform/gke-kubernetes-pipeline-with-cloud-build/08-test-rollback-function.sh
@@ -0,0 +1,17 @@
+# ---------------------------------- #
+#
+# Test the rollback capabilities
+#
+# ---------------------------------- #
+
+:'
+In this task, you rollback to the version of the application that said "Hello World!".
+
+1. In the Cloud console, go to Cloud Build > Dashboard.
+
+2. Click on View all link under Build History for the hello-cloudbuild-env repository.
+
+3. Click on the second most recent build available.
+
+4. Click Rebuild.
+'
