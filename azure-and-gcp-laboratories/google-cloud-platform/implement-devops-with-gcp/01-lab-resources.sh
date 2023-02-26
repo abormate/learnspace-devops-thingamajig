e04aa62d3d39b67b9b9db1ed6d3c362a7b14d6ba
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-27 07:18:38 +0800

    01 - Implement DevOps - Provision Resources

--- azure-and-gcp-laboratories/google-cloud-platform/implement-devops-with-gcp/01-lab-resources.sh
+++ azure-and-gcp-laboratories/google-cloud-platform/implement-devops-with-gcp/01-lab-resources.sh
@@ -24,4 +24,25 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
 --member=serviceAccount:$(gcloud projects describe $PROJECT_ID \
 --format="value(projectNumber)")@cloudbuild.gserviceaccount.com --role="roles/container.developer"
 
+# Configure Git with Cloud Shell, replace <email> with generated laboratory email address
+
+git config --global user.email <email> 
+git config --global user.name <name>
+
+# Create an Artifact Registry Docker repo named "my-repository" in the "us-central1" region to store your container images
+
+# Create a Kubernetes cluster with GKE named "hello-cluster" with the following config
+
+:'
+
+Setting	            Value
+Zone	            us-central1-a
+Release channel	    Regular
+Cluster version	    1.25.5-gke.2000 or newer
+Cluster autoscaler	Enabled
+Number of nodes	    3
+Minimum nodes	    2
+Maximum nodes	    6
+'
+
 
