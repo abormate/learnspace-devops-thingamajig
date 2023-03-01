7a607b069b4286dad9aec23dfd6904af26f5a615
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-02 03:57:02 +0800

    04 - Implement DevOps - Deploy 1st versions App

---
+++ azure-and-gcp-laboratories/google-cloud-platform/implement-devops-with-gcp/04-deploy-1st-version-app.sh
@@ -0,0 +1,22 @@
+# ------------------------------------- #
+#
+# Deploy 1st versions of the application
+#
+# ------------------------------------- #
+
+# In this section, you will build the first version of the production application 
+# and the development application.
+
+# Build the first dev deployment
+
+# step 01 --
+:'
+In Cloud Shell, inspect the cloudbuild-dev.yaml file to see the steps in the build process. 
+Fill in the <version> on lines 10 and 15 with v1.0.
+'
+
+# step 02 --
+:'
+Navigate to the dev/deployment.yaml file and fill in the <todo> on line 17 with the correct 
+container image name.
+'
