c4c9fca7283ce03c406d51e37fcac94d1bed8381
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-01 18:15:45 +0800

    02 - Implement DevOps - Cloud Source Repo - GCP

---
+++ azure-and-gcp-laboratories/google-cloud-platform/implement-devops-with-gcp/02-cloud-source-repositories.sh
@@ -0,0 +1,22 @@
+# ------------------------------------ #
+#
+# Create Cloud Source repositories for end-to-end CI/CD pipeline
+#
+# ------------------------------------ #
+
+:'
+In this task, you will create a repository sample-app in Cloud Source Repositories and will initialize it 
+with some sample code. This repository will hold your Go application code, and be the primary source for 
+triggering builds.
+'
+
+# step 01 -- create empty repository named "sample-app" in Cloud Source
+
+# step 02 -- clone the "sample-app" Cloud Source repo with Cloud Shell
+
+# step 03 -- copy sample code into your sample-app directory, with following command
+
+cd ~
+gsutil cp -r gs://spls/gsp330/sample-app/* sample-app
+
+
