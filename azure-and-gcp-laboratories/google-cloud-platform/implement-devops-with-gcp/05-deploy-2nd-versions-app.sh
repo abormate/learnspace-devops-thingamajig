f62850bec8807dfb7ce68231f3a33e785ed6f5c5
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-03 10:48:32 +0800

    05 - Implement DevOps - Deploy 2nd versions App

--- azure-and-gcp-laboratories/google-cloud-platform/implement-devops-with-gcp/05-deploy-2nd-versions-app.sh
+++ azure-and-gcp-laboratories/google-cloud-platform/implement-devops-with-gcp/05-deploy-2nd-versions-app.sh
@@ -6,5 +6,14 @@
 
 # Build second development deployment
 
-# 01 step --
+# 01 Step --
 # Switch back to the dev branch.
+
+# 02 Step -- 
+# In the main.go file, update the main() function to the following
+func main() {
+	http.HandleFunc("/blue", blueHandler)
+	http.HandleFunc("/red", redHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
