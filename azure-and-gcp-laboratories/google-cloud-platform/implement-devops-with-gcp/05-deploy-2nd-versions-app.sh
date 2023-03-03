7bc14ed381ecd449660a082f522b656919437264
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-03 10:47:14 +0800

    05 - Implement DevOps - Deploy 2nd version app

---
+++ azure-and-gcp-laboratories/google-cloud-platform/implement-devops-with-gcp/05-deploy-2nd-versions-app.sh
@@ -0,0 +1,10 @@
+# ------------------------------------ #
+#
+# Deploy second versions of application
+#
+# ------------------------------------ #
+
+# Build second development deployment
+
+# 01 step --
+# Switch back to the dev branch.
