9650aca4fd7d5ae0babc4a6d4b78a6090772dfff
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-09 18:24:37 +0800

    Training - Implement DevOps - GKE deployment

---
+++ azure-and-gcp-laboratories/google-cloud-platform/implement-devops-with-gcp/comms.sh
@@ -0,0 +1,11 @@
+
+
+gcloud artifacts repositories create my-repository \
+  --repository-format=docker \
+  --location=us-central1
+
+gcloud container clusters create hello-cluster --num-nodes 3 --region us-central1-a --enable-autoscaling --max-nodes 6 --min-nodes 2 --release-channel regular
+
+kubectl create namespace prod && \
+kubectl create namespace dev
+
