f670880a69d93b1233d34426b8c5bafe806c286b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-10 11:34:14 +0800

    Training - DevOps Kode Kloud - Version Control

---
+++ kodekloud-laboratories/version-control-with-git/guide-01.sh
@@ -0,0 +1,17 @@
+# ---------------------------------- #
+#
+# Install version control Git
+#
+# ---------------------------------- #
+
+# identify the version of Linux you're at
+cat /etc/os-release
+
+# or 
+
+cat /etc/*release*
+
+# install git on Debian-based distro like Ubuntu 
+
+sudo apt install git-all
+
