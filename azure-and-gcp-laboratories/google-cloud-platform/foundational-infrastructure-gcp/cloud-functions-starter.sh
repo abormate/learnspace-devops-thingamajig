569f5077fbb1dde7e570ab8339116e8556a0d8a4
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-13 21:03:00 +0800

    Training - Foundational Infrastructure - DevOps - Cloud Functions

--- azure-and-gcp-laboratories/google-cloud-platform/foundational-infrastructure-gcp/cloud-functions-starter.sh
+++ azure-and-gcp-laboratories/google-cloud-platform/foundational-infrastructure-gcp/cloud-functions-starter.sh
@@ -3,3 +3,14 @@
 # Cloud Functions - quick start
 #
 # ------------------------------- #
+
+# What is serverless?
+: '
+Cloud Functions removes the work of managing servers, configuring software, updating frameworks, 
+and patching operating systems. The software and infrastructure are fully managed by Google so that 
+you just add code. Furthermore, provisioning of resources happens automatically in response to events. 
+
+This means that a function can scale from a few invocations a day to many millions of invocations without 
+any work from you.
+'
+
