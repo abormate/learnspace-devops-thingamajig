69b7efb9a987e67de6216fec949f3be3f6ea36bc
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-14 13:44:45 +0800

    Hands-on - DevOps SRE - Reliable Infra - Cloud Logs

---
+++ azure-and-gcp-laboratories/google-cloud-platform/foundational-infrastructure-gcp/05_view-logs.sh
@@ -0,0 +1,7 @@
+# ------------------------------------ #
+#
+# View Logs - to check if function worked
+#
+# ------------------------------------ #
+
+# Check the logs to see your messages in the log history:
