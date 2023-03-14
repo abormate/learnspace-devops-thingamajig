c242d568aa9f3f4a2f06984fc1695426c2a2e009
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-14 13:31:35 +0800

    Hands-on Training - DevOps SRE - Foundational Infrastructure - Cloud Functions

--- azure-and-gcp-laboratories/google-cloud-platform/foundational-infrastructure-gcp/03_function-deployment.sh
+++ azure-and-gcp-laboratories/google-cloud-platform/foundational-infrastructure-gcp/03_function-deployment.sh
@@ -21,4 +21,24 @@ gcloud functions deploy helloWorld \
   --trigger-topic hello_world \
   --runtime nodejs8
 
-  
+# NOTE --> If you get an OperationError, ignore the warning and re-run the command
+
+# If prompted, enter "Y" to allow unauthenticated invocations of a new function
+
+# verify status of function
+
+gcloud functions describe helloWorld
+
+# an active status indicates that the function has been deployed
+
+entryPoint: helloWorld
+eventTrigger:
+  eventType: providers/cloud.pubsub/eventTypes/topic.publish
+  failurePolicy: {}
+  resource:
+...
+status: ACTIVE
+...
+
+# Every message published in the topic triggers function execution, the message contents are passed as input data
+
