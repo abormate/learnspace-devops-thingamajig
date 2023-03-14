f40fdee26fb0e8478a296f006a51d99f4d294338
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-14 13:46:49 +0800

    HandsOn - DevOps SRE - Cloud Infra - Cloud Functions - Logs

--- azure-and-gcp-laboratories/google-cloud-platform/foundational-infrastructure-gcp/05_view-logs.sh
+++ azure-and-gcp-laboratories/google-cloud-platform/foundational-infrastructure-gcp/05_view-logs.sh
@@ -5,3 +5,13 @@
 # ------------------------------------ #
 
 # Check the logs to see your messages in the log history:
+gcloud functions logs read helloWorld
+
+# If the function executed successfully, the messsages in the log will appear as follows:
+LEVEL  NAME        EXECUTION_ID  TIME_UTC                 LOG
+D      helloWorld  3zmhpf7l6j5b  2017-12-05 22:17:42.585  Function execution started
+I      helloWorld  3zmhpf7l6j5b  2017-12-05 22:17:42.650  My Cloud Function: Hello World!
+D      helloWorld  3zmhpf7l6j5b  2017-12-05 22:17:42.666  Function execution took 81 ms, finished with status: 'ok'
+
+# Note: The logs will take around 10 mins to appear. Also, the alternative way to view the logs is, 
+# go to Logging > Logs Explorer.
