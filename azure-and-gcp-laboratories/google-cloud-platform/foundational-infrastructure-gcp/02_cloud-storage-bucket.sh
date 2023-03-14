365de962e6b040a8848983e65cd04024dca8f7a9
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-14 12:24:28 +0800

    Training - DevOps - Foundational Infrastructure - Cloud Storage

---
+++ azure-and-gcp-laboratories/google-cloud-platform/foundational-infrastructure-gcp/02_cloud-storage-bucket.sh
@@ -0,0 +1,9 @@
+# ------------------------------------ #
+#
+# Cloud Storage bucket for Cloud Functions
+#
+# ------------------------------------ #
+
+# Create a new cloud storage bucket for your function
+
+gsutil mb -p [PROJECT_ID] gs://[BUCKET_NAME]
