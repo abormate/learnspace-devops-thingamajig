5d066fbe404564a8b9f9e203bda3054b124e370b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-18 17:28:35 +0800

    DevOps - Cloud Engineering - Pub Sub fundamentals

--- azure-and-gcp-laboratories/google-cloud-platform/gcp-pub-sub-cli/01-pub-sub-topics.sh
+++ azure-and-gcp-laboratories/google-cloud-platform/gcp-pub-sub-cli/01-pub-sub-topics.sh
@@ -25,6 +25,23 @@ gcloud auth list
 # list project ID with the following
 gcloud config list project
 
+: '
+As stated earlier, Google Cloud Pub/Sub is an asynchronous global messaging service. There are three terms in 
+Pub/Sub that appear often: topics, publishing, and subscribing.
+
+A topic is a shared string that allows applications to connect with one another through a common thread.
+
+Publishers push (or publish) a message to a Cloud Pub/Sub topic.
+
+Subscribers make a "subscription" to a topic where they will either pull messages from the subscription or 
+configure webhooks for push subscriptions. Every subscriber must acknowledge each message within a configurable 
+window of time.
+
+To sum it up, a producer publishes messages to a topic and a consumer creates a subscription to a topic to 
+receive messages from it.
+
+'
+
 
 
 
