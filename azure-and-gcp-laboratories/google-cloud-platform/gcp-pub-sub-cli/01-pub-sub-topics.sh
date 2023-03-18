2df05ff36246154f0013f103f954323a414ecd4d
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-18 17:24:05 +0800

    DevOps - Cloud Engineering - Pub Sub Topics

--- azure-and-gcp-laboratories/google-cloud-platform/gcp-pub-sub-cli/01-pub-sub-topics.sh
+++ azure-and-gcp-laboratories/google-cloud-platform/gcp-pub-sub-cli/01-pub-sub-topics.sh
@@ -5,4 +5,14 @@
 # ----------------------------------- #
 
 # Learn 
+# Setting up
+
+# list active account name
+gcloud auth list
+
+# authorize
+
+# list project ID with the following
+gcloud config list project
+
 
