abfcef35dbe9f7abdd5e2006f800e9819eb30de0
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-18 17:25:21 +0800

    DevOps - Cloud Engineering - Pub Sub fundamentals

--- azure-and-gcp-laboratories/google-cloud-platform/gcp-pub-sub-cli/01-pub-sub-topics.sh
+++ azure-and-gcp-laboratories/google-cloud-platform/gcp-pub-sub-cli/01-pub-sub-topics.sh
@@ -7,6 +7,15 @@
 # Learn 
 # Setting up
 
+: '
+Google Cloud Pub/Sub is a messaging service for exchanging event data among applications and services. 
+By decoupling senders and receivers, it allows for secure and highly available communication between 
+independently written applications. Google Cloud Pub/Sub delivers low-latency/durable messaging, and 
+is commonly used by developers in implementing asynchronous workflows, distributing event notifications, 
+and streaming data from various processes or devices
+
+'
+
 # list active account name
 gcloud auth list
 
