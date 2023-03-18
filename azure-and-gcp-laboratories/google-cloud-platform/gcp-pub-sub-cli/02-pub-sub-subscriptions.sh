fe653468e99ae985d2d0bad72b5ba014fdc83b43
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-18 17:45:19 +0800

    DevOps - Cloud Engineering - Pub/Sub subscriptions

---
+++ azure-and-gcp-laboratories/google-cloud-platform/gcp-pub-sub-cli/02-pub-sub-subscriptions.sh
@@ -0,0 +1,8 @@
+# ----------------------------------- #
+#
+# Pub/Sub subscriptions
+#
+# ----------------------------------- #
+
+# Run the following to create a subscription called "mySubscription" to topic "myTopic"
+gcloud  pubsub subscriptions create --topic myTopic mySubscription
