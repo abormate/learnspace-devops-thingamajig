a269bb67b7f6417637858f33afc96483400046fc
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-20 08:44:09 +0800

    03 - DevOps - Cloud Engineering - Pub/Sub Message Publishing

--- azure-and-gcp-laboratories/google-cloud-platform/gcp-pub-sub-cli/03-pub-sub-publish-pull-message.sh
+++ azure-and-gcp-laboratories/google-cloud-platform/gcp-pub-sub-cli/03-pub-sub-publish-pull-message.sh
@@ -9,3 +9,10 @@
 # Publish the message "hello" to the topic you created previously, "myTopic"
 gcloud pubsub topics publish myTopic --message "Hello"
 
+# Publish a few more messages. Replace <your name> with your name and <food> with a food you like:
+gcloud pubsub topics publish myTopic --message "Publisher's name is <YOUR NAME>"
+
+gcloud pubsub topics publish myTopic --message "Publisher likes the <FOOD>"
+
+gcloud pubsub topics publish myTopic --message "Publisher thinks Pub/Sub is awesome"
+
