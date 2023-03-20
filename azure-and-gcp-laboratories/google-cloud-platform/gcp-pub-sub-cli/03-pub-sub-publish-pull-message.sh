5f67c93be5721be41831206c04fda732cfd6f93c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-20 08:41:55 +0800

    DevOps - Cloud Engineering - Pub Sub messages

---
+++ azure-and-gcp-laboratories/google-cloud-platform/gcp-pub-sub-cli/03-pub-sub-publish-pull-message.sh
@@ -0,0 +1,11 @@
+# ------------------------------------ #
+#
+# Pub/Sub publishing and pulling a message
+#
+# ------------------------------------ #
+
+# Learn
+
+# Publish the message "hello" to the topic you created previously, "myTopic"
+gcloud pubsub topics publish myTopic --message "Hello"
+
