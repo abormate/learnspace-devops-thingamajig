676355842a02ff289f77ddf4f60d0d83d4712c64
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-25 20:06:08 +0800

    Cloud Monitoring - VM instance

--- multi-cloud-projects/google-cloud-platform/cloud-monitoring-setup/01-vm-monitoring-collect-write.sh
+++ multi-cloud-projects/google-cloud-platform/cloud-monitoring-setup/01-vm-monitoring-collect-write.sh
@@ -6,3 +6,18 @@
 
 # Learn 
 
+# Create a VM instance. Go to the navigation menu of the Console > select Compute Engine > VM instances
+
+# Fill in the fields, leave other fields with their default values.
+
+: '
+Field	        Value
+Name	        lamp-1-vm
+Region	        us-central1
+Zone	        us-central1-a
+Series	        N1
+Machine type	n1-standard-2
+Boot disk	    Click Change. Select version Debian GNU/Linux 10 (buster) for Debian OS and click Select.
+Firewall	    check Allow HTTP traffic
+
+'
