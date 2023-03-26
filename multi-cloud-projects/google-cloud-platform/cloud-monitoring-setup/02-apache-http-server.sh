a2826b9d8c866b0815591bc527f6a14749de3ccd
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-27 06:33:01 +0800

    Cloud Monitoring - Compute Engine - Apache HTTP

---
+++ multi-cloud-projects/google-cloud-platform/cloud-monitoring-setup/02-apache-http-server.sh
@@ -0,0 +1,8 @@
+# ------------------------------- #
+#
+#  Install Apache 2 HTTP server to your Compute instance 
+#
+# ------------------------------- #
+
+# From the console, SSH to "lamp-1-vm" to open a terminal to your instance
+
