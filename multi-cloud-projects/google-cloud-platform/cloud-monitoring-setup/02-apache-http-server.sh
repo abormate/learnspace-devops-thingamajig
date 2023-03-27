9236c9ae247dee13700e7a4fa24f555e3deba48b
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-28 06:01:50 +0800

    Cloud Monitoring - DevOps - Compute Engine - Logging and Monitoring agents

--- multi-cloud-projects/google-cloud-platform/cloud-monitoring-setup/02-apache-http-server.sh
+++ multi-cloud-projects/google-cloud-platform/cloud-monitoring-setup/02-apache-http-server.sh
@@ -15,3 +15,5 @@ sudo apt-get install apache2 php7.0
 # If you have trouble installing PHP 7.0 -- then you can try instead, PHP 5
 
 sudo service apache2 restart
+
+# Now, you'll need to install monitoring and logging agents to your server(s)
