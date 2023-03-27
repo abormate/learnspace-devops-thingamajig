cae549c5ec8188f1ce2d3a3a24c7a4e76780337c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-28 06:14:22 +0800

    Compute Engine - Uptime Checker

---
+++ multi-cloud-projects/google-cloud-platform/cloud-monitoring-setup/03-uptime-check.sh
@@ -0,0 +1,9 @@
+# ------------------------------- #
+#
+# Create an Uptime check
+#
+# ------------------------------- #
+
+# Uptime checks verify that a resource is always accessible. 
+# For practice, create an uptime check to verify your VM is up
+
