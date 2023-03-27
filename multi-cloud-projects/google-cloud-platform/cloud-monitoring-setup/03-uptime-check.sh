3d606ad05f107207ea252454c2a68d96a970f739
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-28 06:15:52 +0800

    DevOps - Compute Engine - Uptime Checker

--- multi-cloud-projects/google-cloud-platform/cloud-monitoring-setup/03-uptime-check.sh
+++ multi-cloud-projects/google-cloud-platform/cloud-monitoring-setup/03-uptime-check.sh
@@ -7,3 +7,7 @@
 # Uptime checks verify that a resource is always accessible. 
 # For practice, create an uptime check to verify your VM is up
 
+# From Cloud Console -- go to, "Uptime checks" --> +Create Uptime Check
+
+# Set the following fields
+
