c24bca2eca56c7c25a70425e6cc061b66f66bbbb
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-29 08:41:11 +0800

    AWS - DevOps - Install WordPress to EC2 instance

---
+++ multi-cloud-projects/aws-projects/wordpress-dynamic-site-deploy/ec2-install-wordpress-setup.sh
@@ -0,0 +1,6 @@
+#1. create the html directory and mount the efs to it
+sudo su
+yum update -y
+mkdir -p /var/www/html
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport <efs-endpoint-url>1.amazonaws.com:/ /var/www/html
+
