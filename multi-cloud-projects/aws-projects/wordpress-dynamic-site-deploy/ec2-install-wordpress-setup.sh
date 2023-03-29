200dc56c061139402f8ff1135fa1a2a61afef4de
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-29 08:44:50 +0800

    AWS - DevOps - Install WordPress to EC2 instance

--- multi-cloud-projects/aws-projects/wordpress-dynamic-site-deploy/ec2-install-wordpress-setup.sh
+++ multi-cloud-projects/aws-projects/wordpress-dynamic-site-deploy/ec2-install-wordpress-setup.sh
@@ -22,3 +22,9 @@ sudo yum install mysql-community-server -y
 sudo systemctl enable mysqld
 sudo systemctl start mysqld
 
+#5. set permissions
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+sudo find /var/www -type f -exec sudo chmod 0664 {} \;
+chown apache:apache -R /var/www/html
