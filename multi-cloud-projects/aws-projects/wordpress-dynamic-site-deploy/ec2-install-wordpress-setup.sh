504b669d2fa27a5a07b4658b6625c57b7420ea1e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-29 08:41:42 +0800

    AWS - DevOps - Install WordPress to EC2

--- multi-cloud-projects/aws-projects/wordpress-dynamic-site-deploy/ec2-install-wordpress-setup.sh
+++ multi-cloud-projects/aws-projects/wordpress-dynamic-site-deploy/ec2-install-wordpress-setup.sh
@@ -4,3 +4,8 @@ yum update -y
 mkdir -p /var/www/html
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport <efs-endpoint-url>1.amazonaws.com:/ /var/www/html
 
+#2. install apache 
+sudo yum install -y httpd httpd-tools mod_ssl
+sudo systemctl enable httpd 
+sudo systemctl start httpd
+
