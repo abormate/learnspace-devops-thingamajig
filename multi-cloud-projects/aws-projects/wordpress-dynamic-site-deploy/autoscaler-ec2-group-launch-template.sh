36fe77ff70847e3017ad4875d390d89b3306be1f
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-30 19:39:25 +0800

    AWS DevOps - WordPress

--- multi-cloud-projects/aws-projects/wordpress-dynamic-site-deploy/autoscaler-ec2-group-launch-template.sh
+++ multi-cloud-projects/aws-projects/wordpress-dynamic-site-deploy/autoscaler-ec2-group-launch-template.sh
@@ -7,6 +7,12 @@ sudo yum install -y httpd httpd-tools mod_ssl
 sudo systemctl enable httpd
 
 sudo systemctl start httpd
+<<<<<<< HEAD
+
+sudo yum install php php-common php-pear -y
+sudo yum install php-{cgi,curl,mbstring,gd,mysqlnd,gettext,json,xml,fpm,intl,zip} -y
+=======
 
 sudo amazon-linux-extras enable php7.4
 sudo yum clean metadata
+>>>>>>> daeaf148fa13cecb9e865848245435f6d4f41330
