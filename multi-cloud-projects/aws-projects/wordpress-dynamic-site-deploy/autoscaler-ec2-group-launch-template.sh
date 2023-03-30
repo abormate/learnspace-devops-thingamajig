609a1a7ad8159e6dd42024b9b611c7fe73216d04
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-30 19:40:36 +0800

    AWS DevOps - WordPress

--- multi-cloud-projects/aws-projects/wordpress-dynamic-site-deploy/autoscaler-ec2-group-launch-template.sh
+++ multi-cloud-projects/aws-projects/wordpress-dynamic-site-deploy/autoscaler-ec2-group-launch-template.sh
@@ -10,3 +10,6 @@ sudo systemctl start httpd
 
 sudo yum install php php-common php-pear -y
 sudo yum install php-{cgi,curl,mbstring,gd,mysqlnd,gettext,json,xml,fpm,intl,zip} -y
+
+sudo rpm -Uvh https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
