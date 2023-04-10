a7c9c18936a718b488a206e59c2990b829536e78
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-04-10 20:28:50 +0800

    Cloud Engineering - GCP - Kubernetes Deployments

---
+++ multi-cloud-projects/google-cloud-platform/cloud-engineer-learnspace/setup-cloud-environment-gcp/managing-deployments-kubernetes.sh
@@ -0,0 +1,7 @@
+# Introduction to deployments
+: '
+Heterogeneous deployments typically involve connecting two or more distinct infrastructure environments or 
+regions to address a specific technical or operational need. Heterogeneous deployments are called "hybrid", 
+"multi-cloud", or "public-private", depending upon the specifics of the deployment.
+'
+
