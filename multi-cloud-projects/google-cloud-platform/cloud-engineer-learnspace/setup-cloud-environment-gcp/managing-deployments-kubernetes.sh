f11ddc408c14baf45fc8ca45711c3acd9d2d4dba
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-04-15 11:08:02 +0800

    Cloud Engineering - GCP - Kubernetes Deployment

--- multi-cloud-projects/google-cloud-platform/cloud-engineer-learnspace/setup-cloud-environment-gcp/managing-deployments-kubernetes.sh
+++ multi-cloud-projects/google-cloud-platform/cloud-engineer-learnspace/setup-cloud-environment-gcp/managing-deployments-kubernetes.sh
@@ -151,4 +151,5 @@ kubectl get deployments
 kubectl get replicasets
 
 # We should see a ReplicaSet with a name like "auth-xxxxxxx"
- 
+# Finally, we can view the Pods that were created as part of our deployment. 
+# The single Pod is created by the Kubernetes when the ReplicaSet is created:
