495d834d4f3b19c6dd27dd917a3ac1acd64ceadc
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-04-16 18:20:35 +0800

    Cloud Engineering - Deployments for Kubernetes Cluster

--- multi-cloud-projects/google-cloud-platform/cloud-engineer-learnspace/setup-cloud-environment-gcp/managing-deployments-kubernetes.sh
+++ multi-cloud-projects/google-cloud-platform/cloud-engineer-learnspace/setup-cloud-environment-gcp/managing-deployments-kubernetes.sh
@@ -167,4 +167,6 @@ kubectl create -f services/hello.yaml
 kubectl create secret generic tls-certs --from-file tls/
 kubectl create configmap nginx-frontend-conf --from-file=nginx/frontend.conf
 kubectl create -f deployments/frontend.yaml
+kubectl create -f services/frontend.yaml
+
 
