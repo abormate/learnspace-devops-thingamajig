8b5aea454f4706b5b9ab810cc522324bd7f123f2
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-11-03 13:19:05 +0800

    DevOps Labs -- Docker

--- kodekloud-laboratories/docker-kubernetes/thor-jump.sh
+++ kodekloud-laboratories/docker-kubernetes/thor-jump.sh
@@ -8,5 +8,10 @@ applications. The DevOps team wants to do some testing per the following:
 # Tasked to install docker-ce on CentOS server
 sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+# Install docker compose on CentOS server
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+
 # Start docker-ce
 sudo systemctl start docker
+
