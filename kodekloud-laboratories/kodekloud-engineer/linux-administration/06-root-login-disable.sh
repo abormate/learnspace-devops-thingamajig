b278dd596f8acec442243fdadb64285535481f61
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-16 15:37:19 +0800

    KodeKloud Engineer -- Linux -- Disable Root

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/06-root-login-disable.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/06-root-login-disable.sh
@@ -4,3 +4,13 @@
 #
 # ------------------------------------------ #
 
+: '
+After doing some security audits of servers, xCorp Industries security 
+team has implemented some new security policies. One of them is to 
+disable direct root login through SSH.
+
+
+Disable direct SSH root login on all app servers in Stratos Datacenter.
+
+'
+
