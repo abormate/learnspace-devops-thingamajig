5e614dcefa909a29dceb52a3ec39a1b49576b8bd
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-18 12:31:31 +0800

    KodeKloud Engineer -- Linux -- Archives

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/07-linux-archives.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/07-linux-archives.sh
@@ -4,3 +4,20 @@
 #
 # --------------------------------------- #
 
+: '
+On Nautilus storage server in Stratos DC, there is a storage location 
+named /data, which is used by different developers to keep their data 
+(non confidential data). 
+
+One of the developers named ravi has raised a 
+ticket and asked for a copy of their data present in /data/ravi directory 
+on storage server. /home is a FTP location on storage server itself 
+where developers can download their data. Below are the instructions 
+shared by the system admin team to accomplish this task.
+
+
+
+a. Make a ravi.tar.gz compressed archive of /data/ravi directory and move 
+the archive to /home directory on Storage Server.
+
+'
