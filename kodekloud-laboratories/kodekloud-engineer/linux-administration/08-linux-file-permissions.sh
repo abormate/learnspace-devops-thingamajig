b2c354993da3221e1dbb345833c40264b438a771
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-12-29 21:23:11 +0800

    KodeKloud Engineer -- Linux -- File Permissions

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/08-linux-file-permissions.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/08-linux-file-permissions.sh
@@ -14,4 +14,7 @@ script xfusioncorp.sh. They have already copied the script on all
 required servers, however they did not make it executable on one the 
 app server i.e App Server 1 in Stratos Datacenter.
 
+Please give executable permissions to /tmp/xfusioncorp.sh script on App 
+Server 1. Also make sure every user can execute it.
+
 '
