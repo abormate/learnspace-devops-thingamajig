09f2e6616966ca8109835461d61d9260e47bde7e
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-06 14:19:57 +0800

    Kode Kloud Engineer -- Linux -- String Substitution

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/10-linux-string-substitute.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/10-linux-string-substitute.sh
@@ -14,4 +14,10 @@ be populated with valid data before they can be used.
 One of the daily tasks of a system admin working in the xFusionCorp 
 industries is to apply string and file manipulation commands!
 
+-- Note --
+
+Replace all occurances of the string Random to Echo-Location on the XML 
+file /root/nautilus.xml located in the backup server.
+
+
 '
