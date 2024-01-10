7a1206f87cd51289fe4d33587946b5bff1b92691
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-10 16:17:31 +0800

    Kode Kloud Engineer -- Linux -- File Copy Remotely

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/11-linux-copy-file-remote.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/11-linux-copy-file-remote.sh
@@ -4,4 +4,16 @@
 #
 # -------------------------------------------- #
 
+: '
+One of the Nautilus devs has copied condifential data on the jump host in
+Stratos DC. That data must be copied to one of the app servers. Because the devs
+do not have access to app servers. They asked the system admins to accomplish
+the task for them
+
+Copy /tmp/nautilus.txt.gpg from jump server to app server 3
+at location /home/code
+
+
+'
+
 scp /tmp/nautilus.txt.gpg banner@stapp03: /home/code
