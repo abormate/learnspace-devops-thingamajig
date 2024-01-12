00bb6cabb9ddd27106c7e20a343c01ef4f9e4ff4
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-12 14:51:29 +0800

    Kode Kloud Engineer -- Linux -- Cron Schedule Deny Access

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/12-linux-cron-schedule.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/12-linux-cron-schedule.sh
@@ -13,3 +13,8 @@ below specified users on App Server 1.
 Allow crontab access to ammar user and deny the same to garrett user.
 
 '
+
+vim /etc/cron.deny
+
+# add users to deny -- one per each line from file 
+
