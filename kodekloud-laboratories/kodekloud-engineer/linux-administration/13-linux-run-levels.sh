e4813fcc8ceed7193e2f45efaa81c6ab5ba34ed9
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-15 19:52:51 +0800

    Kode Kloud Engineer -- Linux -- Run Levels

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/13-linux-run-levels.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/13-linux-run-levels.sh
@@ -4,3 +4,12 @@
 #
 # ---------------------------------------------- #
 
+: '
+New tools have been installed on the app servers in Stratos Datacenter. 
+Some of these tools can only be managed from the graphical user interface. 
+
+Therefore, there are some requirements for these app servers as below.
+
+
+
+'
