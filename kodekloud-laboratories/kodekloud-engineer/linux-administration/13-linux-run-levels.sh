023211f88be5035bce7e2d05671f667a82aebd43
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-01-15 19:59:18 +0800

    Kode Kloud Engineer -- Linux -- Run Levels Target

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/13-linux-run-levels.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/13-linux-run-levels.sh
@@ -15,3 +15,13 @@ that they can boot in GUI (graphical user interface) by default. Please
 do not try to reboot these servers after completing this task.
 
 '
+
+sudo su -
+
+systemctl get-default
+
+systemctl list-units --type=target --all
+
+systemctl set-default graphical.target
+
+# run commands as root on all app servers
