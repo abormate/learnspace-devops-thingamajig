fa273cfa27994f063c954033b7d94d8af4965119
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-01 17:08:54 +0800

    Kode Kloud Engineer -- Linux -- Firewall Rules

---
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/16-linux-firewalld-rules.sh
@@ -0,0 +1,16 @@
+# ------------------------------------------- #
+#
+# -- Linux Kode Kloud -- Firewall Rules --
+#
+# ------------------------------------------- #
+
+: '
+The Nautilus system admins team recently deployed a web UI application 
+for their backup utility running on the Nautilus backup server in Stratos 
+Datacenter. The application is running on port 5003. They have firewalld 
+installed on that server. The requirements that have come up include the 
+following:
+
+Open all incoming connection on 5003/tcp port. Zone should be public.
+
+'
