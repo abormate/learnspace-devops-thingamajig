430fce4d3b7f4ca8fcdd39d341234474d2821da5
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-07 16:04:40 +0800

    Kode Kloud Playground -- Azure and Terraform

--- kodekloud-laboratories/terraform/terraform-and-azure/01-kode-kloud-playground-terraform.tf
+++ kodekloud-laboratories/terraform/terraform-and-azure/01-kode-kloud-playground-terraform.tf
@@ -4,3 +4,12 @@
 #
 # -------------------------------------- #
 
+/*
+With this playground, developers can easily experiment, test, and learn 
+with real-world examples of how to provision and manage infrastructure on 
+Azure using Terraform, without incurring costs associated with creating 
+resources on a live Azure account.
+
+
+
+*/
