2775afe5db763756f8c71c2adc8df10c1d62210c
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-08 16:23:21 +0800

    Google Cloud -- Terraform Project 001 -- Fundamentals Infra

--- kodekloud-laboratories/terraform/terraform-google-cloud/gcp-project-01/main.tf
+++ kodekloud-laboratories/terraform/terraform-google-cloud/gcp-project-01/main.tf
@@ -18,3 +18,17 @@ resource "google_storage_object_access_control" "public_rule" {
 #  role   = "READER"
 #  entity = "allUsers"
 #  }
+
+# Upload the html file to the bucket
+resource "google_storage_bucket_object" "static_site_src" {
+  name   = "index.html"
+  source = "../website/index.html"
+  bucket = google_storage_bucket.website.name
+  
+}
+
+# Reserve an external IP
+resource "google_compute_global_address" "website" {
+  provider = google
+  name     = "website-lb-ip"
+}
