95adf65eda301eaabd8998e9d6774156d029bb9f
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-11 14:53:01 +0800

    Terraform IaC -- GCP Infra -- Providers

--- kodekloud-laboratories/terraform/terraform-google-cloud/gcp-project-01/provider.tf
+++ kodekloud-laboratories/terraform/terraform-google-cloud/gcp-project-01/provider.tf
@@ -6,3 +6,9 @@ provider "google" {
   region       = var.gcp_region
 }
 
+# GCP beta provider
+provider "google-beta" {
+  credentials  = file(var.gcp_svc_key)
+  project      = var.gcp_project
+  region       = var.gcp_region
+}
