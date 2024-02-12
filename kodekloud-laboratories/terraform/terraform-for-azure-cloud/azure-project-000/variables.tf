7466846eff607e86109ad05142bb083f5a8a0bbf
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-12 10:26:47 +0800

    Terraform -- Azure Cloud

--- kodekloud-laboratories/terraform/terraform-for-azure-cloud/azure-project-000/variables.tf
+++ kodekloud-laboratories/terraform/terraform-for-azure-cloud/azure-project-000/variables.tf
@@ -9,3 +9,15 @@ variable "resource_group_name_prefix" {
   default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
+
+variable "node_count" {
+  type        = number
+  description = "The initial quantity of nodes for the node pool."
+  default     = 3
+}
+
+variable "username" {
+  type        = string
+  description = "The admin username for the new cluster."
+  default     = "azureadmin"
+}
