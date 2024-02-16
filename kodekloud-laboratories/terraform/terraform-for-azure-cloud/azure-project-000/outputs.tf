010ed62525e685e0af496cdc0b35266509ef1a37
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-16 13:41:58 +0800

    Terraform -- Azure Cloud

--- kodekloud-laboratories/terraform/terraform-for-azure-cloud/azure-project-000/outputs.tf
+++ kodekloud-laboratories/terraform/terraform-for-azure-cloud/azure-project-000/outputs.tf
@@ -15,3 +15,8 @@ output "client_key" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_key
   sensitive = true
 }
+
+output "cluster_ca_certificate" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
+  sensitive = true
+}
