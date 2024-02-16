a93ad35a8d508c5cf747925fb9d5fb44717420d3
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-16 13:42:37 +0800

    Terraform Infra -- Azure Cloud

--- kodekloud-laboratories/terraform/terraform-for-azure-cloud/azure-project-000/outputs.tf
+++ kodekloud-laboratories/terraform/terraform-for-azure-cloud/azure-project-000/outputs.tf
@@ -25,3 +25,8 @@ output "cluster_password" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config[0].password
   sensitive = true
 }
+
+output "cluster_username" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].username
+  sensitive = true
+}
