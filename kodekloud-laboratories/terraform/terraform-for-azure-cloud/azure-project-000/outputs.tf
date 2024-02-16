02cc4f671c3cfa0fc65b306c3c168d1313d5a8fe
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-16 13:43:41 +0800

    Terraform Infra -- Azure Cloud -- Outputs Config File

--- kodekloud-laboratories/terraform/terraform-for-azure-cloud/azure-project-000/outputs.tf
+++ kodekloud-laboratories/terraform/terraform-for-azure-cloud/azure-project-000/outputs.tf
@@ -30,3 +30,13 @@ output "cluster_username" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config[0].username
   sensitive = true
 }
+
+output "host" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].host
+  sensitive = true
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
+  sensitive = true
+}
