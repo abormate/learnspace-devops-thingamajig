a8716919f272f3aeca9a9b9246afdfbd3f099d0d
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-23 10:13:47 +0800

    Kode Kloud Engineer -- Kubernetes -- Create Pods inside Cluster

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/01-scenario-pods-create.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/01-scenario-pods-create.sh
@@ -76,3 +76,58 @@ kubectl get pods
 
 kubectl describe pod pod-nginx
 
+# must output the following describe
+: '
+Name:             pod-nginx
+Namespace:        default
+Priority:         0
+Service Account:  default
+Node:             kodekloud-control-plane/172.17.0.2
+Start Time:       Fri, 23 Feb 2024 02:10:53 +0000
+Labels:           app=nginx_app
+Annotations:      <none>
+Status:           Running
+IP:               10.244.0.5
+IPs:
+  IP:  10.244.0.5
+Containers:
+  nginx-container:
+    Container ID:   containerd://f2cc4b54e9b1c089371a69d0bca59303d997baee70a75e8c9524a27f13ed71d2
+    Image:          nginx:latest
+    Image ID:       docker.io/library/nginx@sha256:c26ae7472d624ba1fafd296e73cecc4f93f853088e6a9c13c0d52f6ca5865107
+    Port:           <none>
+    Host Port:      <none>
+    State:          Running
+      Started:      Fri, 23 Feb 2024 02:11:03 +0000
+    Ready:          True
+    Restart Count:  0
+    Environment:    <none>
+    Mounts:
+      /var/run/secrets/kubernetes.io/serviceaccount from kube-api-access-db22b (ro)
+Conditions:
+  Type              Status
+  Initialized       True 
+  Ready             True 
+  ContainersReady   True 
+  PodScheduled      True 
+Volumes:
+  kube-api-access-db22b:
+    Type:                    Projected (a volume that contains injected data from multiple sources)
+    TokenExpirationSeconds:  3607
+    ConfigMapName:           kube-root-ca.crt
+    ConfigMapOptional:       <nil>
+    DownwardAPI:             true
+QoS Class:                   BestEffort
+Node-Selectors:              <none>
+Tolerations:                 node.kubernetes.io/not-ready:NoExecute op=Exists for 300s
+                             node.kubernetes.io/unreachable:NoExecute op=Exists for 300s
+Events:
+  Type    Reason     Age   From               Message
+  ----    ------     ----  ----               -------
+  Normal  Scheduled  113s  default-scheduler  Successfully assigned default/pod-nginx to kodekloud-control-plane
+  Normal  Pulling    112s  kubelet            Pulling image "nginx:latest"
+  Normal  Pulled     103s  kubelet            Successfully pulled image "nginx:latest" in 8.821052242s (8.821065601s including waiting)
+  Normal  Created    103s  kubelet            Created container nginx-container
+  Normal  Started    103s  kubelet            Started container nginx-container
+
+'
