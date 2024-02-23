efe5393c5798e62e9edfdb10f04c9653ec28d9b8
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-23 09:25:00 +0800

    Kode Kloud Engineer Training -- SE Linux Installation

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/18-linux-selinux-distro-installation.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/18-linux-selinux-distro-installation.sh
@@ -8,4 +8,9 @@
 The xFusionCorp Industries security team recently did a security audit of their 
 infrastructure and came up with ideas to improve the application and server security. 
 
+They decided to use SElinux for an additional security layer. They are still planning 
+how they will implement it; however, they have decided to start testing with app 
+servers, so based on the recommendations they have the following requirements:
+
+
 '
