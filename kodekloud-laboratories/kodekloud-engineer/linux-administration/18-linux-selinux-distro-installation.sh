1594882991d12d2bff602a26d51929058e7e93af
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-23 09:23:54 +0800

    Kode Kloud -- Linux

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/18-linux-selinux-distro-installation.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/18-linux-selinux-distro-installation.sh
@@ -4,3 +4,8 @@
 #
 # ---------------------------------------------- #
 
+: '
+The xFusionCorp Industries security team recently did a security audit of their 
+infrastructure and came up with ideas to improve the application and server security. 
+
+'
