a2990001957335bec91f1bbae667e27ef4556e0c
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-26 16:25:24 +0800

    Kode Kloud Engineer

---
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/02-create-deployments.sh
@@ -0,0 +1,15 @@
+# --------------------------------------- #
+#
+# -- Create Kubernetes Deployments --
+#
+# --------------------------------------- #
+
+: '
+The Nautilus DevOps team has started practicing some pods and services deployment on 
+Kubernetes platform, as they are planning to migrate most of their applications on 
+Kubernetes. Recently one of the team members has been assigned a task to create a 
+deployment as per details mentioned below:
+
+
+
+'
