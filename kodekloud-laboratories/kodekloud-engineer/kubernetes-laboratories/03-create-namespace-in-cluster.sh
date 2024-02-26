fd9003f229c94a38256767fe58fa808e399c3836
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-27 02:53:56 +0800

    Kode Kloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/03-create-namespace-in-cluster.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/03-create-namespace-in-cluster.sh
@@ -4,3 +4,10 @@
 #
 # ------------------------------------------------ #
 
+: '
+The Nautilus DevOps team is planning to deploy some micro services on Kubernetes 
+platform. The team has already set up a Kubernetes cluster and now they want set up 
+some namespaces, deployments etc. Based on the current requirements, the team has 
+shared some details as below:
+
+'
