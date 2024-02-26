10e886ea18082be9776e063c781582d9c2c1a584
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-02-27 02:54:32 +0800

    kode Kloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/03-create-namespace-in-cluster.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/03-create-namespace-in-cluster.sh
@@ -10,4 +10,10 @@ platform. The team has already set up a Kubernetes cluster and now they want set
 some namespaces, deployments etc. Based on the current requirements, the team has 
 shared some details as below:
 
+Create a namespace named dev and create a POD under it; name the pod dev-nginx-pod and 
+use nginx image with latest tag only and remember to mention tag i.e nginx:latest.
+
+Note: The kubectl utility on jump_host has been configured to work with the kubernetes 
+cluster.
+
 '
