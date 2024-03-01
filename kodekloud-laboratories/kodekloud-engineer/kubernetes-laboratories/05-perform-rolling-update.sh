364091d71e5c02f82bef9df60ff73a4e5db046dd
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-01 13:04:33 +0800

    Kode Kloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/05-perform-rolling-update.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/05-perform-rolling-update.sh
@@ -4,3 +4,12 @@
 #
 # ------------------------------------------ #
 
+: '
+We have an application running on Kubernetes cluster using nginx web server. The 
+Nautilus application development team has pushed some of the latest changes and those 
+changes need be deployed. The Nautilus DevOps team has created an image nginx:1.17 with 
+the latest changes.
+
+
+
+'
