64346c3ee0f8078552b2f7f03b38719dfa691d19
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-01 13:05:33 +0800

    Kode Kloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/05-perform-rolling-update.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/05-perform-rolling-update.sh
@@ -10,6 +10,12 @@ Nautilus application development team has pushed some of the latest changes and 
 changes need be deployed. The Nautilus DevOps team has created an image nginx:1.17 with 
 the latest changes.
 
+Perform a rolling update for this application and incorporate nginx:1.17 image. The 
+deployment name is nginx-deployment
 
+Make sure all pods are up and running after the update.
+
+Note: The kubectl utility on jump_host has been configured to work with the kubernetes 
+cluster.
 
 '
