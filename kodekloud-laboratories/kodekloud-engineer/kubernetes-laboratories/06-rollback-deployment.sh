ea3d6f5c1975e2544a4a55510883ed7b8505ef06
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-04 08:08:53 +0800

    Kode Kloud -- Kubernetes -- Rollback

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/06-rollback-deployment.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/06-rollback-deployment.sh
@@ -4,3 +4,11 @@
 #
 # -------------------------------------------------- #
 
+: '
+This morning the Nautilus DevOps team rolled out a new release for one of the 
+applications. Recently one of the customers logged a complaint which seems to be about 
+a bug related to the recent release. Therefore, the team wants to rollback the recent 
+release.
+
+'
+
