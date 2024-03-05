19275d5e8a3bb10d014432c2ee573d491759e1d2
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-05 13:15:47 +0800

    Kode Kloud Engineer -- Kubernetes -- Replicaset

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/07-replicaset-creation.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/07-replicaset-creation.sh
@@ -12,6 +12,12 @@ they are planning to migrate some of their existing applications there. Recently
 of the team members has been assigned a task to write a template as per details 
 mentioned below:
 
+>> 1. Create a ReplicaSet using httpd image with latest tag only and remember to mention 
+tag i.e httpd:latest and name it as httpd-replicaset.
 
+>> 2. Labels app should be httpd_app, labels type should be front-end.
+
+>> 3. The container should be named as httpd-container; also make sure replicas counts 
+are 4.
 
 '
