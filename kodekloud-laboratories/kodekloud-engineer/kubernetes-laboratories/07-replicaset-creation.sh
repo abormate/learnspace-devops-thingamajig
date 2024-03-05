ab93f6bbdad7f9be3f8050726c545d1409a64a3a
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-05 13:14:49 +0800

    Kode Kloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/07-replicaset-creation.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/07-replicaset-creation.sh
@@ -4,3 +4,14 @@
 #
 # ----------------------------------------------- #
 
+: '
+Context necessary --
+
+The Nautilus DevOps team is going to deploy some applications on kubernetes cluster as 
+they are planning to migrate some of their existing applications there. Recently one 
+of the team members has been assigned a task to write a template as per details 
+mentioned below:
+
+
+
+'
