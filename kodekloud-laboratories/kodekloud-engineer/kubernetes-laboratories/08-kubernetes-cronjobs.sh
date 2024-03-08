89cfe953724fec5848615907f621bce3d385b651
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-08 13:34:34 +0800

    Kode Kloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/08-kubernetes-cronjobs.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/08-kubernetes-cronjobs.sh
@@ -6,6 +6,28 @@
 
 # Go to official Kubernetes documentation page and check template for Cronjobs
 
+: '
+apiVersion: batch/v1
+kind: CronJob
+metadata:
+  name: hello
+spec:
+  schedule: "* * * * *"
+  jobTemplate:
+    spec:
+      template:
+        spec:
+          containers:
+          - name: hello
+            image: busybox:1.28
+            imagePullPolicy: IfNotPresent
+            command:
+            - /bin/sh
+            - -c
+            - date; echo Hello from the Kubernetes cluster
+          restartPolicy: OnFailure
+'
+
 # save template modified with correct details into file --> cronjob.yaml
 
 kubectl create -f cronjob.yaml
