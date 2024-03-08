cc7229a11c1e03e7cd026755727186740ae95e82
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-08 13:40:39 +0800

    KodeKloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/09-countdown-job-kubernetes.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/09-countdown-job-kubernetes.sh
@@ -4,3 +4,22 @@
 #
 # ------------------------------------------ #
 
+# Look into documentation for Kubernetes for a regular Job template
+
+: '
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: pi
+spec:
+  template:
+    spec:
+      containers:
+      - name: pi
+        image: perl:5.34.0
+        command: ["perl",  "-Mbignum=bpi", "-wle", "print bpi(2000)"]
+      restartPolicy: Never
+  backoffLimit: 4
+
+
+'
