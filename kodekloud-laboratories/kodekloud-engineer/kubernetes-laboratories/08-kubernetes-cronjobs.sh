e5cf1e82705e4139edd5361cea2f490edc3b9f1a
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-08 12:48:39 +0800

    Kode Kloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/08-kubernetes-cronjobs.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/08-kubernetes-cronjobs.sh
@@ -4,3 +4,7 @@
 #
 # ------------------------------------------ #
 
+# Go to official Kubernetes documentation page and check template for Cronjobs
+
+# save template modified with correct details into file --> cronjob.yaml
+
