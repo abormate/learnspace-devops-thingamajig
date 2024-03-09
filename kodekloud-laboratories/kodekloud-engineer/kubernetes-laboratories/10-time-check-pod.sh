2242cf4630503217ffec4490adf7afe24d4dce44
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-09 09:46:30 +0800

    Kode Kloud Engineer -- Kubernetes -- Config Map Time Check Pod

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/10-time-check-pod.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/10-time-check-pod.sh
@@ -4,3 +4,23 @@
 #
 # ------------------------------------------------ #
 
+kubectl get namespace 
+
+kubectl create namespace <name>
+
+kubectl get namespace
+
+# get config map from Kubernetes documentation
+
+: '
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: game-demo
+data:
+  # property-like keys; each key maps to a simple value
+  player_initial_lives: "3"
+  ui_properties_file_name: "user-interface.properties"
+
+
+'
