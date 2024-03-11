3ec6890a4b26d660b20ce322b48192813e389c45
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-11 13:28:41 +0800

    Kode Kloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/08-kubernetes-cronjobs.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/08-kubernetes-cronjobs.sh
@@ -4,6 +4,18 @@
 #
 # ------------------------------------------ #
 
+: '
+There are some jobs/tasks that need to be run regularly on different schedules. 
+
+Currently the Nautilus DevOps team is working on developing some scripts that will be 
+executed on different schedules, but for the time being the team is creating some cron 
+jobs in Kubernetes cluster with some dummy commands (which will be replaced by original 
+scripts later). 
+
+Create a cronjob as per details given below:
+
+'
+
 # Go to official Kubernetes documentation page and check template for Cronjobs
 
 : '
