51ebac0e951d3da6e911077555d106b53b432704
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-15 12:48:59 +0800

    Kode Kloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/11-pod-troubleshooting.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/11-pod-troubleshooting.sh
@@ -5,9 +5,15 @@
 # ------------------------------------------------- #
 
 : '
-One of the junior DevOps team members was working on to deploy a 
+One of the DevOps team members was working on to deploy a 
 stack on Kubernetes cluster. Somehow the pod is not coming up and 
 its failing with some errors. We need to fix this as soon as 
 possible. Please look into it.
+
+>> There is a pod named webserver and the container under it is named 
+as httpd-container. It is using image httpd:latest
+
+>> There is a sidecar container as well named sidecar-container which 
+is using ubuntu:latest image.
 
 '
