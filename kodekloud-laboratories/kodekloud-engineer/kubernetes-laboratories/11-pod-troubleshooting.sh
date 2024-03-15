a7a7baea2aec3d4fca7a9b4e3fb9f36363a703b0
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-15 12:47:13 +0800

    Kode Kloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/11-pod-troubleshooting.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/11-pod-troubleshooting.sh
@@ -4,3 +4,10 @@
 #
 # ------------------------------------------------- #
 
+: '
+One of the junior DevOps team members was working on to deploy a 
+stack on Kubernetes cluster. Somehow the pod is not coming up and 
+its failing with some errors. We need to fix this as soon as 
+possible. Please look into it.
+
+'
