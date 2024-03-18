3db1c847c725d6f1fe8f35a4174af05c2840dc3d
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-18 12:47:51 +0800

    Kode Kloud Engineer -- Kubernetes

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/12-update-deployments.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/12-update-deployments.sh
@@ -4,3 +4,12 @@
 #
 # -------------------------------------- #
 
+: '
+There is an application deployed on Kubernetes cluster. Recently, 
+the Nautilus application development team developed a new version 
+of the application that needs to be deployed now. As per new 
+updates some new changes need to be made in this existing setup. 
+
+So update the deployment and service as per details mentioned below:
+
+'
