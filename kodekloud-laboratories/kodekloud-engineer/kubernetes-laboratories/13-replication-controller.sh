513846b9f081377df392fea8009e38c047701108
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-19 11:22:00 +0800

    Kode Kloud Engineer -- Kubernetes -- Replication Controller

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/13-replication-controller.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/13-replication-controller.sh
@@ -25,4 +25,25 @@ All pods should be running state after deployment.
 
 # go through Kubernetes documentation -- and look for replication controller template -- that creates nginx resources
 
+: '
+apiVersion: v1
+kind: ReplicationController
+metadata:
+  name: nginx
+spec:
+  replicas: 3
+  selector:
+    app: nginx
+  template:
+    metadata:
+      name: nginx
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx
+        ports:
+        - containerPort: 80
 
+'
