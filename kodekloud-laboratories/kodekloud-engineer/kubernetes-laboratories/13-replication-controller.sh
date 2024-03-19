0f25c151236e65ca5c6e5fce6aae928fe36e2dbb
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-19 11:13:07 +0800

    Kode Kloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/13-replication-controller.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/13-replication-controller.sh
@@ -4,5 +4,13 @@
 #
 # ---------------------------------------------------- #
 
-The Nautilus DevOps team wants to create a ReplicationController to deploy several pods. They are going to deploy applications on these pods, these applications need highly available infrastructure. Below you can find exact details, create the ReplicationController accordingly.
+: '
+The Nautilus DevOps team wants to create a ReplicationController to 
+deploy several pods. They are going to deploy applications on these 
+pods, these applications need highly available infrastructure. 
+
+Below you can find exact details, create the ReplicationController 
+accordingly.
+
+'
 
