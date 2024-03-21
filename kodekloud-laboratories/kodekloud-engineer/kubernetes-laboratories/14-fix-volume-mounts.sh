f54e58e0ab64748967e6dddb13ed0ea66628d13c
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-21 16:49:42 +0800

    Kode Kloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/14-fix-volume-mounts.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/14-fix-volume-mounts.sh
@@ -8,4 +8,8 @@
 We deployed a Nginx and PHP-FPM based setup on Kubernetes cluster 
 last week and it had been working fine so far. 
 
+This morning one of the team members made a change somewhere which 
+caused some issues, and it stopped working. Please look into the 
+issue and fix it:
+
 '
