68748eaf2fdad96b4c2b4831649f42219db778bd
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-21 16:51:20 +0800

    Kode Kloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/14-fix-volume-mounts.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/14-fix-volume-mounts.sh
@@ -16,5 +16,10 @@ issue and fix it:
 The pod name is nginx-phpfpm and configmap name is nginx-config. 
 Figure out the issue and fix the same.
 
+Once issue is fixed, copy /home/thor/index.php file from the jump 
+host to the nginx-container under nginx document root and you 
+should be able to access the website using Website button on top 
+bar.
+
 
 '
