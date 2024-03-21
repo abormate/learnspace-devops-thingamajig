4c41dd7f3677f7e343c22f8d1c42fbef9d8b811f
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-21 16:50:49 +0800

    Kode Kloud Engineer

--- kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/14-fix-volume-mounts.sh
+++ kodekloud-laboratories/kodekloud-engineer/kubernetes-laboratories/14-fix-volume-mounts.sh
@@ -12,4 +12,9 @@ This morning one of the team members made a change somewhere which
 caused some issues, and it stopped working. Please look into the 
 issue and fix it:
 
+
+The pod name is nginx-phpfpm and configmap name is nginx-config. 
+Figure out the issue and fix the same.
+
+
 '
