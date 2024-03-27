d3229c383f4ccdeb1fd801c41f2a885e1da9a104
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-27 15:54:55 +0800

    Kode Kloud Engineer -- Ansible -- Modules for Copy to Multiple Servers

--- kodekloud-laboratories/kodekloud-engineer/ansible/04-ansible-module-copy.sh
+++ kodekloud-laboratories/kodekloud-engineer/ansible/04-ansible-module-copy.sh
@@ -10,4 +10,18 @@ in Stratos DC. Nautilus DevOps team want to perform this task using Ansible.
 
 Perform the task as per details mentioned below:
 
+a. On jump host create an inventory file /home/thor/ansible/inventory and add 
+all application servers as managed nodes.
+
+
+b. On jump host create a playbook /home/thor/ansible/playbook.yml to copy 
+/usr/src/sysops/index.html file to all application servers at location 
+/opt/sysops.
+
+
+Note: Validation will try to run the playbook using command ansible-playbook 
+-i inventory playbook.yml so please make sure the playbook works this way 
+without passing any extra arguments.
+
+
 '
