b4bc7122810895b78c6cfe46bf5c7858c77ff703
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-03-27 15:53:34 +0800

    Kode Kloud Engineer -- Ansible

--- kodekloud-laboratories/kodekloud-engineer/ansible/04-ansible-module-copy.sh
+++ kodekloud-laboratories/kodekloud-engineer/ansible/04-ansible-module-copy.sh
@@ -4,3 +4,10 @@
 #
 # --------------------------------------------- #
 
+: '
+There is data on jump host that needs to be copied on all application servers 
+in Stratos DC. Nautilus DevOps team want to perform this task using Ansible. 
+
+Perform the task as per details mentioned below:
+
+'
