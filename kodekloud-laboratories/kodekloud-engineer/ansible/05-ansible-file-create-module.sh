6ec6c08a1c4357b8db0614b48c2861202e0e33e0
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-02 10:02:50 +0800

    Kode Kloud Engineer -- Ansible -- File Module

--- kodekloud-laboratories/kodekloud-engineer/ansible/05-ansible-file-create-module.sh
+++ kodekloud-laboratories/kodekloud-engineer/ansible/05-ansible-file-create-module.sh
@@ -4,3 +4,9 @@
 #
 # --------------------------------------------- #
 
+: '
+The Nautilus DevOps team is working to test several Ansible modules on servers 
+in Stratos DC. Recently they wanted to test the file creation on remote hosts 
+using Ansible. Find below more details about the task:
+
+'
