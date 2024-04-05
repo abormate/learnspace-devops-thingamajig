4458db399eb236dc076258a03bfafa3e152d24c1
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-05 20:45:23 +0800

    Docker -- Overview

--- kodekloud-laboratories/docker-kubernetes/docker-fundamentals/01-overview.sh
+++ kodekloud-laboratories/docker-kubernetes/docker-fundamentals/01-overview.sh
@@ -5,7 +5,11 @@
 # --------------------------------------------- #
 
 : '
+Allows self contained containers to have isolated environments
+for various components of an applications. With its own set of libraries
+and other dependencies that the component requires. 
 
+It's running under the same OS kernel
 
 
 '
