f7ffead879cec1720e4cb947a304e030749b2b4f
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-05 20:46:55 +0800

    Docker -- Containerization Overview

--- kodekloud-laboratories/docker-kubernetes/docker-fundamentals/01-overview.sh
+++ kodekloud-laboratories/docker-kubernetes/docker-fundamentals/01-overview.sh
@@ -9,7 +9,8 @@ Allows self contained containers to have isolated environments
 for various components of an applications. With its own set of libraries
 and other dependencies that the component requires. 
 
-It's running under the same OS kernel
-
+Its running under the same OS kernel. Unlike before when the
+putting together of various components that require different
+and specific dependencies tend to be problematic to say the least. 
 
 '
