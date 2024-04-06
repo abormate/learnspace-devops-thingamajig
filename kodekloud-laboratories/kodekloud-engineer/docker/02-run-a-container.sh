e23321541eff5d43f6bfd0ee15857aaee09c225f
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-06 09:33:36 +0800

    Kode Kloud -- Docker -- Run Container

--- kodekloud-laboratories/kodekloud-engineer/docker/02-run-a-container.sh
+++ kodekloud-laboratories/kodekloud-engineer/docker/02-run-a-container.sh
@@ -4,3 +4,10 @@
 #
 # ------------------------------------- #
 
+: '
+Nautilus DevOps team is testing some applications deployment on 
+some of the application servers. They need to deploy a nginx 
+container on Application Server 2. Please complete the task as 
+per details given below:
+
+'
