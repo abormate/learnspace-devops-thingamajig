ad8746b5e0a9496b5805eaa664fbfcbaf69378cd
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-07 17:11:09 +0800

    Kode Kloud Engineer -- Docker -- Container Delete

--- kodekloud-laboratories/kodekloud-engineer/docker/03-delete-container.sh
+++ kodekloud-laboratories/kodekloud-engineer/docker/03-delete-container.sh
@@ -4,3 +4,13 @@
 #
 # ----------------------------------------- #
 
+: '
+One of the Nautilus project developers created a container on App 
+Server 1. This container was created for testing only and now we 
+need to delete it. Accomplish this task as per details given 
+below:
+
+Delete a container named kke-container, its running on App 
+Server 1 in Stratos DC.
+
+'
