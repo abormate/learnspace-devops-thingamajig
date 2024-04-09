41b037eafe6d1e1c35a9dd42bf66a749119f918e
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-09 08:49:46 +0800

    Kode Kloud -- Source Version Control

--- kodekloud-laboratories/kodekloud-engineer/versioning-and-source-control/02-clone-repo.sh
+++ kodekloud-laboratories/kodekloud-engineer/versioning-and-source-control/02-clone-repo.sh
@@ -12,5 +12,9 @@ DC.
 
 Please clone the repo as per details shared below:
 
+1. The repo that needs to be cloned is /opt/beta.git
+
+2. Clone this git repository under /usr/src/kodekloudrepos 
+directory. Please do not try to make any changes in the repo.
 
 '
