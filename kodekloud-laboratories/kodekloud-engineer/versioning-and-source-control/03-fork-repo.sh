8cbf540530bebf8dfdc34cee6c713a598aa4e45f
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-09 09:04:15 +0800

    Kode Kloud -- Source Control

--- kodekloud-laboratories/kodekloud-engineer/versioning-and-source-control/03-fork-repo.sh
+++ kodekloud-laboratories/kodekloud-engineer/versioning-and-source-control/03-fork-repo.sh
@@ -5,6 +5,12 @@
 # ---------------------------------------------- #
 
 : '
+There is a Git server used by the Nautilus project teams. Recently 
+a new developer Jon joined the team and needs to start working on a 
+project. To do so, he needs to fork an existing Git repository. 
+
+Below you can find more details:
+
 
 
 '
