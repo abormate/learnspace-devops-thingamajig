b4f55ef6f17ba3eb03981af1b07800b1c6c4911f
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-09 08:48:46 +0800

    Kode Kloud -- Source Control

--- kodekloud-laboratories/kodekloud-engineer/versioning-and-source-control/02-clone-repo.sh
+++ kodekloud-laboratories/kodekloud-engineer/versioning-and-source-control/02-clone-repo.sh
@@ -4,3 +4,13 @@
 #
 # -------------------------------------------- #
 
+: '
+DevOps team created a new Git repository last week; however, as of 
+now no team is using it. The Nautilus application development team 
+recently asked for a copy of that repo on Storage server in Stratos 
+DC. 
+
+Please clone the repo as per details shared below:
+
+
+'
