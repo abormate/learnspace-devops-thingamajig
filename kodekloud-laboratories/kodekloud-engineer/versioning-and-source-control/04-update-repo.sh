243272f91b80faf8404611a5290ef89ee1b5f59e
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-10 12:51:27 +0800

    Kode Kloud -- Source Control -- Update Repo

--- kodekloud-laboratories/kodekloud-engineer/versioning-and-source-control/04-update-repo.sh
+++ kodekloud-laboratories/kodekloud-engineer/versioning-and-source-control/04-update-repo.sh
@@ -4,3 +4,14 @@
 #
 # ---------------------------------------------- #
 
+: '
+The Nautilus development team started with new project development. 
+They have created different Git repositories to manage respective 
+projects source code. One of the repositories /opt/cluster.git 
+was created recently. The team has given us a sample index.html 
+file that is currently present on jump host under /tmp directory. 
+
+The repository has been cloned at /usr/src/kodekloudrepos on storage server in Stratos DC.
+
+
+'
