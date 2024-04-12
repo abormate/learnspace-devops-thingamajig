f26e86de19095a4e540ed43ca0538ca65e5e039e
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-12 10:40:10 +0800

    Kode Kloud -- Jenkins -- Install and Configure Server

---
+++ kodekloud-laboratories/kodekloud-engineer/jenkins/01-install-jenkins-server.sh
@@ -0,0 +1,22 @@
+# ------------------------------------------ #
+#
+# -- Install and Configure -- Jenkins -- 
+#
+# ------------------------------------------ #
+
+: '
+The DevOps team of xFusionCorp Industries is planning to setup some 
+CI/CD pipelines. After several meetings they have decided to use 
+Jenkins server. 
+
+So, we need to setup a Jenkins Server as soon as possible. 
+Please complete the task as per requirements mentioned below:
+
+1. Install jenkins on jenkins server using yum utility only, and 
+start its service. You might face timeout issue while starting the 
+Jenkins service, please refer this link for help.
+
+2. Jenkins admin user name should be theadmin, password should be ***, 
+full name should be Rose and email should be ***@****.com.
+
+'
