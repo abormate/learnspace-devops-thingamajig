84f21b03b00d36022458ab46265c69e225547ea6
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-18 10:33:47 +0800

    Jenkins -- CI CD

--- kodekloud-laboratories/kodekloud-engineer/jenkins/03-jenkins-user-permissions.sh
+++ kodekloud-laboratories/kodekloud-engineer/jenkins/03-jenkins-user-permissions.sh
@@ -3,3 +3,13 @@
 # -- Jenkins -- CI/CD -- Users Management --
 #
 # --------------------------------------------- #
+
+: '
+The Nautilus team is planning to use Jenkins for some of their CI/CD 
+pipelines. DevOps team has just installed a fresh Jenkins server and 
+they are configuring it further to be available for use.
+
+Click on the Jenkins button on the top bar to access the Jenkins UI. 
+Login using username admin and password Adm!n321.
+
+'
