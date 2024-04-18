9e33dc0c100c5515581ef84462283659fc392b84
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-18 10:35:20 +0800

    Jenkins -- CI/CD

--- kodekloud-laboratories/kodekloud-engineer/jenkins/03-jenkins-user-permissions.sh
+++ kodekloud-laboratories/kodekloud-engineer/jenkins/03-jenkins-user-permissions.sh
@@ -12,4 +12,8 @@ they are configuring it further to be available for use.
 Click on the Jenkins button on the top bar to access the Jenkins UI. 
 Login using username admin and password Adm!n321.
 
+It has only a sample job for now. A new developer has joined the 
+Nautilus application development team and they want this user to be 
+added to the Jenkins server as per the details mentioned below:
+
 '
