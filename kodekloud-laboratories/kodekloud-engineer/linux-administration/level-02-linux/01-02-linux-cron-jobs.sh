72bc84b8413d03f8fbdb4ebbad1ced92f3109a3f
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-19 17:45:26 +0800

    Kode Kloud -- Linux 02 -- Cron Jobs

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/01-02-linux-cron-jobs.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/01-02-linux-cron-jobs.sh
@@ -4,3 +4,11 @@
 #
 # ---------------------------------------- #
 
+: '
+The Nautilus system admins team has prepared scripts to automate 
+several day-to-day tasks. They want them to be deployed on all app 
+servers in Stratos DC on a set schedule. Before that they need to 
+test similar functionality with a sample cron job. Therefore, 
+perform the steps below:
+
+'
