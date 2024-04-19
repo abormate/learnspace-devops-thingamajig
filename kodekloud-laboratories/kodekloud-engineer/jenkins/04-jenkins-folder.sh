6ab0adb592dd66ae280413d5fb704615c05e00a2
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-19 18:41:47 +0800

    Kode Kloud -- Jenkins CI/CD - Organize Folders

--- kodekloud-laboratories/kodekloud-engineer/jenkins/04-jenkins-folder.sh
+++ kodekloud-laboratories/kodekloud-engineer/jenkins/04-jenkins-folder.sh
@@ -4,3 +4,15 @@
 #
 # ------------------------------------------ #
 
+: '
+The DevOps team of xFusionCorp Industries is planning to create a 
+number of Jenkins jobs for different tasks. To easily manage the 
+jobs within Jenkins UI they decided to create different Folders 
+for all Jenkins jobs based on usage/nature of these jobs. 
+
+Based on the requirements shared below please perform the below 
+mentioned task:
+
+Click on the Jenkins button on the top bar to access the Jenkins UI. Login using username admin and password Adm!n321.
+
+'
