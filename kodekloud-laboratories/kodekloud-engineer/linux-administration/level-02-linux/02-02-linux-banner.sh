ee474666546c928a6eb3f01e5481af495744e048
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-20 13:26:19 +0800

    Kode Kloud -- Jenkins -- CI CD -- Banners

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/02-02-linux-banner.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/02-02-linux-banner.sh
@@ -3,3 +3,13 @@
 # -- Linux Banner -- Kode Kloud -- 
 #
 # ------------------------------------------- #
+
+: '
+During the monthly compliance meeting, it was pointed out that 
+several servers in the Stratos DC do not have a valid banner. 
+
+The security team has provided serveral approved templates 
+which should be applied to the servers to maintain compliance. 
+These will be displayed to the user upon a successful login.
+
+'
