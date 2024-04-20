2e0a59066b52ab5021cd43efb861c946e85b6056
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-20 13:27:05 +0800

    Kode Kloud -- Jenkins CI CD -- Banners

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/02-02-linux-banner.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/02-02-linux-banner.sh
@@ -12,4 +12,8 @@ The security team has provided serveral approved templates
 which should be applied to the servers to maintain compliance. 
 These will be displayed to the user upon a successful login.
 
+Update the message of the day on all application and db servers for
+Nautilus. Make use of the approved template located at 
+/home/thor/nautilus_banner on jump host
+
 '
