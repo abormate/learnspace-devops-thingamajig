91913ea7d1287f3cb26ae6a1c4e2392bd9792a43
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-27 17:45:47 +0800

    Linux SystemOps -- Collaborative Directories

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/03-02-collaborative-directories.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/03-02-collaborative-directories.sh
@@ -12,4 +12,9 @@ to be strictly accessed by the sysadmin group of the team.
 Setup a collaborative directory /sysadmin/data on app server 2 in 
 Stratos Datacenter.
 
+The directory should be group owned by the group sysadmin and the 
+group should own the files inside the directory. The directory 
+should be read/write/execute to the user and group owners, and 
+others should not have any access.
+
 '
