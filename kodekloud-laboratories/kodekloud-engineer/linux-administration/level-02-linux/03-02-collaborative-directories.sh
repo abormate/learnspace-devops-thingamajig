edb897ecc47dc5c7b4e3171af215ae89e4519cd8
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-04-27 17:31:32 +0800

    Linux SysOps -- Collaborative Directories

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/03-02-collaborative-directories.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/03-02-collaborative-directories.sh
@@ -9,4 +9,7 @@ The Nautilus team doesnt want its data to be accessed by any of
 the other groups/teams due to security reasons and want their data 
 to be strictly accessed by the sysadmin group of the team.
 
+Setup a collaborative directory /sysadmin/data on app server 2 in 
+Stratos Datacenter.
+
 '
