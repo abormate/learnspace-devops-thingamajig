b4e5088bdff0c92be136564f87530f688ecec971
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-02 12:37:07 +0800

    Linux

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/04-02-linux-string-substitute.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/04-02-linux-string-substitute.sh
@@ -10,5 +10,9 @@ Data needs to be altered in several of the files. On Nautilus
 App Server 3, alter the /home/BSD.txt file as per details given 
 below:
 
+a. Delete all lines containing word copyright and save results in 
+/home/BSD_DELETE.txt file. (Please be aware of case sensitivity)
+
+
 
 '
