150aee7d44b01108f6730d6cdb63babfdcf8a0bf
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-02 12:36:43 +0800

    Linux -- SysOps

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/04-02-linux-string-substitute.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/04-02-linux-string-substitute.sh
@@ -4,3 +4,11 @@
 #
 # ------------------------------------------------ #
 
+: '
+There is some data on Nautilus App Server 3 in Stratos DC. 
+Data needs to be altered in several of the files. On Nautilus 
+App Server 3, alter the /home/BSD.txt file as per details given 
+below:
+
+
+'
