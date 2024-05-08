b018625af79ab7c564ef6596330a4d66b716e6f5
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-08 19:31:09 +0800

    DevOps Automation -- JIRA ticket

--- small-projects-cloud-thingamajig/python-for-devops/projects/01-jira-ticket-automate.py
+++ small-projects-cloud-thingamajig/python-for-devops/projects/01-jira-ticket-automate.py
@@ -14,3 +14,31 @@ headers = {
   "Accept": "application/json",
   "Content-Type": "application/json"
 }
+
+payload = json.dumps( {
+  "fields": {
+    "description": {
+      "content": [
+        {
+          "content": [
+            {
+              "text": "My first jira ticket",
+              "type": "text"
+            }
+          ],
+          "type": "paragraph"
+        }
+      ],
+      "type": "doc",
+      "version": 1
+    },
+    "project": {
+      "key": "AB"
+    },
+    "issuetype": {
+      "id": "10006"
+    },
+    "summary": "First JIRA Ticket",
+  },
+  "update": {}
+} )
