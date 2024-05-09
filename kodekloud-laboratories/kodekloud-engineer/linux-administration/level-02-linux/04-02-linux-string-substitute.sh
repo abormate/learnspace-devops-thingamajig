6a4239d83306c87d9d94c145a52bbe76ad30852e
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-09 17:41:00 +0800

    Kode Kloud -- Linux SysOps -- String Substitute

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/04-02-linux-string-substitute.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/04-02-linux-string-substitute.sh
@@ -60,3 +60,19 @@ cat  /etc/os-release
         regexp: '\b{{ rep_src_word }}\b'
         replace: "{{ rep_dest_word }}"
 '
+
+: '
+There is some data on Nautilus App Server 2 in Stratos DC. Data needs to be altered in 
+several of the files. On Nautilus App Server 2, alter the /home/BSD.txt file as per 
+details given below:
+
+a. Delete all lines containing word following and save results in /home/BSD_DELETE.txt 
+file. (Please be aware of case sensitivity)
+
+b. Replace all occurrence of word and to them and save results in /home/BSD_REPLACE.txt 
+file.
+
+Note: Let's say you are asked to replace word to with from. In that case, make sure not to alter any words containing the string itself; for example upto, contributor etc.
+
+'
+
