e7d62917c0eac6eedfe6d14fa9ef268557af3921
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-10 15:06:27 +0800

    Kode Kloud -- Linux ServerOps -- SSH

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/05-02-linux-ssh-connect.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/05-02-linux-ssh-connect.sh
@@ -18,3 +18,6 @@ through their respective sudo users.
 '
 
 ssh-keygen -t rsa -b 4096 -f "/home/thor/.ssh/id_rsa" -q -N ""
+
+sshpass -p Ir0nM@n ssh-copy-id -i "/home/thor/.ssh/id_rsa.pub" tony@172.16.238.10 -f -o StrictHostKeyChecking=no
+sshpass -p Am3ric@ ssh-copy-id -i "/home/thor/.ssh/id_rsa.pub" steve@172.16.238.11 -f -o StrictHostKeyChecking=no
