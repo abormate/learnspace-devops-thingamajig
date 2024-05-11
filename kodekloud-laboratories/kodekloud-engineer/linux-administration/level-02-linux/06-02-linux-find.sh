2c9488b822de09b73b5815004a574c157e78c273
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-11 11:12:55 +0800

    Kode Kloud -- Linux

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/06-02-linux-find.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/06-02-linux-find.sh
@@ -5,7 +5,11 @@
 # --------------------------------------------- #
 
 : '
+During a routine security audit, the team identified an issue on the Nautilus App Server. 
+Some malicious content was identified within the website code. After digging into the 
+issue they found that there might be more infected files. 
 
+Before doing a cleanup they
 
 
 '
