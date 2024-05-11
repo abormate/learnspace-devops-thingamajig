9926cf2f153bbdbc7f7852ef656f82eeee6cae0e
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-11 11:13:28 +0800

    Kode Kloud -- Linux ServerOps

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/06-02-linux-find.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/06-02-linux-find.sh
@@ -9,7 +9,7 @@ During a routine security audit, the team identified an issue on the Nautilus Ap
 Some malicious content was identified within the website code. After digging into the 
 issue they found that there might be more infected files. 
 
-Before doing a cleanup they
-
+Before doing a cleanup they would like to find all similar files and copy them to a safe 
+location for further investigation. Accomplish the task as per the following requirements:
 
 '
