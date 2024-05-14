e09cafa2c980773ac088f88b3404d3dfce40bd9a
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-14 11:24:40 +0800

    Linux Laboratory

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/06-02-linux-find.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/06-02-linux-find.sh
@@ -12,4 +12,13 @@ issue they found that there might be more infected files.
 Before doing a cleanup they would like to find all similar files and copy them to a safe 
 location for further investigation. Accomplish the task as per the following requirements:
 
+
+a. On App Server 2 at location /var/www/html/beta find out all files (not directories) 
+having .php extension.
+
+b. Copy all those files along with their parent directory structure to location /beta on 
+same server.
+
+c. Please make sure not to copy the entire /var/www/html/beta directory content.
+
 '
