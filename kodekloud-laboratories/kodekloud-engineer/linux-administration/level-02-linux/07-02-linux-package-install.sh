4ab3d1c6c55985c4c657e5645cdccc9be94ac097
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-19 18:22:28 +0800

    Linux ServerOps -- Install Package

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/07-02-linux-package-install.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/07-02-linux-package-install.sh
@@ -4,3 +4,12 @@
 #
 # -------------------------------------------------- #
 
+: '
+As per new application requirements shared by the Nautilus project development team, 
+several new packages need to be installed on all app servers in Stratos Datacenter. Most of 
+them are completed except for samba.
+
+Therefore, install the samba package on all app-servers.
+
+'
+
