46608e0acfe4929af9ed558b91d9b70a2f446776
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-20 11:41:03 +0800

    Linux ServerOps -- Install Ansible

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/08-02-linux-install-ansible.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/08-02-linux-install-ansible.sh
@@ -11,4 +11,8 @@ options, the team has decided to go with Ansible for now due to its simple setup
 pre-requisites. The team wanted to start testing using Ansible, so they have decided to use 
 jump host as an Ansible controller to test different kind of tasks on rest of the servers.
 
+Install ansible version 4.7.0 on Jump host using pip3 only. Make sure Ansible binary is 
+available globally on this system, i.e all users on this system are able to run Ansible 
+commands.
+
 '
