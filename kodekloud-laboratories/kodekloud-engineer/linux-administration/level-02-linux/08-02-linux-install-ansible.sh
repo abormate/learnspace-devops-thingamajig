99fba3faa9dc74314cec2dd82ad61c4a2896f02c
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-20 11:40:15 +0800

    Linux ServerOps -- Install Ansible

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/08-02-linux-install-ansible.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/08-02-linux-install-ansible.sh
@@ -4,3 +4,11 @@
 #
 # --------------------------------------------------- #
 
+: '
+During the weekly meeting, the Nautilus DevOps team discussed about the automation and 
+configuration management solutions that they want to implement. While considering several 
+options, the team has decided to go with Ansible for now due to its simple setup and minimal 
+pre-requisites. The team wanted to start testing using Ansible, so they have decided to use 
+jump host as an Ansible controller to test different kind of tasks on rest of the servers.
+
+'
