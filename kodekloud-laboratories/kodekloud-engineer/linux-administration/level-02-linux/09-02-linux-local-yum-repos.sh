914e9947d3fd9d5c6003fd3948239e6cc2bca441
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-23 00:14:09 +0800

    Linux ServerOps -- Configure Local Yum Repos

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/09-02-linux-local-yum-repos.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/09-02-linux-local-yum-repos.sh
@@ -11,6 +11,14 @@ they have decided to configure a local yum repo on Nautilus Backup Server. This 
 pending items from last month, so please configure a local yum repository on Nautilus Backup 
 Server as per details given below.
 
+a. We have some packages already present at location /packages/downloaded_rpms/ on 
+Nautilus Backup Server.
 
+
+b. Create a yum repo named local_yum and make sure to set Repository ID to local_yum. 
+Configure it to use packages location /packages/downloaded_rpms/.
+
+
+c. Install package vim-enhanced from this newly created repo.
 
 '
