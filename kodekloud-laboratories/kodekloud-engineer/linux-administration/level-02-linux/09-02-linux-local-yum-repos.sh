7d7d377979641f6b7968912a14bcc0b2e0b2c9a1
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-23 00:13:11 +0800

    Linux ServerOps -- Configure Yum Repos

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/09-02-linux-local-yum-repos.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/09-02-linux-local-yum-repos.sh
@@ -4,3 +4,13 @@
 #
 # --------------------------------------------------- #
 
+: '
+The Nautilus production support team and security team had a meeting last month in which they 
+decided to use local yum repositories for maintaing packages needed for their servers. For now 
+they have decided to configure a local yum repo on Nautilus Backup Server. This is one of the 
+pending items from last month, so please configure a local yum repository on Nautilus Backup 
+Server as per details given below.
+
+
+
+'
