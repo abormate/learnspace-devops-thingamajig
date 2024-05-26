4fbfdb219014b59f4e62962f295df85b424181db
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-26 18:42:42 +0800

    Linux Automation -- with Bash Shell -- Check Disk Usage

--- small-projects-cloud-thingamajig/code-laboratory/server-administration/02-disk-usage-check.sh
+++ small-projects-cloud-thingamajig/code-laboratory/server-administration/02-disk-usage-check.sh
@@ -11,3 +11,6 @@ THRESHOLD=80
 df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '( print $5 " " $1 }' | 
 while read output;
 do
+  usage=$(echo $output | awk '(print $1)' | cut -d'%' -f1)
+  partition=$(echo $output | awk '( print $2 )')
+  
