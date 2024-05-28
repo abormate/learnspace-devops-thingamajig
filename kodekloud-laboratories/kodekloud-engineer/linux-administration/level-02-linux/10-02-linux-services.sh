9abecd0f84e91f34d4aade88614e4d16dd0b660e
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-28 13:42:29 +0800

    Linux ServerOps -- Services

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/10-02-linux-services.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/10-02-linux-services.sh
@@ -4,3 +4,11 @@
 #
 # -------------------------------------------------- #
 
+: '
+As per details shared by the development team, the new application release has some 
+dependencies on the back end. There are some packages/services that need to be installed on 
+all app servers under Stratos Datacenter. As per requirements please perform the following 
+steps:
+
+
+'
