e3d0cb2bfaf7cd8744865d868a7f73bed0c56ea5
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-05-28 13:42:58 +0800

    Linux ServerOps -- Services

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/10-02-linux-services.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/10-02-linux-services.sh
@@ -10,5 +10,8 @@ dependencies on the back end. There are some packages/services that need to be i
 all app servers under Stratos Datacenter. As per requirements please perform the following 
 steps:
 
+a. Install squid package on all the application servers.
+
+b. Once installed, make sure it is enabled to start during boot.
 
 '
