e13bfd87922be487549e0960b1cde38b604a3280
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-07-30 19:48:33 +0800

    Devops -- Linux -- Firewall Setup

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/13-02-firewall-setup.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/13-02-firewall-setup.sh
@@ -5,7 +5,14 @@
 # ------------------------------------ #
 
 : '
-To secure our Nautilus infrastructure in Stratos Datacenter we have decided to install and configure firewalld on all app servers. We have Apache and Nginx services running on these apps. Nginx is running as a reverse proxy server for Apache. We might have more robust firewall settings in the future, but for now we have decided to go with the given requirements listed below:
+To secure our Nautilus infrastructure in Stratos Datacenter we have 
+decided to install and configure firewalld on all app servers. We have 
+Apache and Nginx services running on these apps. Nginx is running as a 
+reverse proxy server for Apache. 
+
+We might have more robust firewall 
+settings in the future, but for now we have decided to go with the 
+given requirements listed below:
 
 
 '
