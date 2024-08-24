0981478fc812efedf2a900741853ba76a3150188
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-08-24 20:30:39 +0800

    DevOps -- Linux Server Security

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/15-02-SE-linux-install.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/15-02-SE-linux-install.sh
@@ -33,3 +33,4 @@ sudo yum install policycoreutils policycoreutils-python selinux-policy selinux-p
 
 # To disable SELinux permanently, modify the SELinux configuration file. Run the following command with root privileges to open the SELinux configuration file in a text editor:
 
+sudo vi /etc/selinux/config
