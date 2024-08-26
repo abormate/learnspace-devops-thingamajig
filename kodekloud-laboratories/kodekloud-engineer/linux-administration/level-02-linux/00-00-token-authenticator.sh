213569bcddcddd08cbaa851a766336dbdf9cf953
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-08-26 19:08:16 +0800

    DevOps -- Cluster Authenticator

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/00-00-token-authenticator.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/00-00-token-authenticator.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 # Assume the role and capture the output
+ROLE_OUTPUT=$(aws sts assume-role --duration-second 28800 \ 
+     -- role-arn "arn:aws:iam::XXXXXXXXXXXX:role/XXXXXXXXX" \
+     -- role-session-name eks-session)
+
+     
