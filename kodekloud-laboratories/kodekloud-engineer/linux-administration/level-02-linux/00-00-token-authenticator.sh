2d32de5641f4f2c8ad19545c60d49cf56d36d782
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-08-26 19:13:08 +0800

    DevOps -- Cluster Authenticator

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/00-00-token-authenticator.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/00-00-token-authenticator.sh
@@ -7,3 +7,4 @@ ROLE_OUTPUT=$(aws sts assume-role --duration-second 28800 \
 
 # Extract credentials from output
 ACCESS_KEY_ID=$(echo $ROLE_OUTPUT | jq -r '.Credentials.AccessKeyId')
+SECRET_ACCESS_KEY=$(echo $ROLE_OUTPUT | jq -r '.Credentials.SecretAccessKey')
