6df9687f18fbaab10fce95b04cbca659989a4877
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-08-26 19:08:38 +0800

    DevOps -- K8s Authenticator

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/00-00-token-authenticator.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/00-00-token-authenticator.sh
@@ -3,6 +3,5 @@
 # Assume the role and capture the output
 ROLE_OUTPUT=$(aws sts assume-role --duration-second 28800 \ 
      -- role-arn "arn:aws:iam::XXXXXXXXXXXX:role/XXXXXXXXX" \
-     -- role-session-name eks-session)
+     -- role-session-name k8s-session)
 
-     
