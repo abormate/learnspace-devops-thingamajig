7e763803291aa7d0b4bc4d4295a73997a3b95d02
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-09-05 19:43:40 +0800

    DevOps -- Linux Server -- Firewall

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/13-02-firewall-setup.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/13-02-firewall-setup.sh
@@ -71,3 +71,4 @@ sudo firewall-cmd --zone=public --remove-port=8080/tcp --permanent
 sudo firewall-cmd --zone=public --list-all
 sudo firewall-cmd --set-default-zone=public
 sudo firewall-cmd --reload
+
