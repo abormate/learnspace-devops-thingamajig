31ee6a30113942f56abaa9b5b86197d0d8af4ac6
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-10-08 02:19:38 +0800

    DevOps StudySpace -- Collective Mirror Servers Config

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/19-02-rev-mirror-server.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/19-02-rev-mirror-server.sh
@@ -12,7 +12,11 @@ need to configure LBR server. There are number of
 options for that, but team has decided to go with 
 HAproxy. 
 
-FYI, apache is running on port 3002 on all 
+FYI, apache is running on port 3001 on all 
 app servers. Complete this task as per below details.
+
+a. Install and configure HAproxy on LBR server using yum only and make sure all app servers are added to HAproxy load balancer. HAproxy must serve on default http port (Note: Please do not remove stats socket /var/lib/haproxy/stats entry from haproxy default config.).
+
+b. Once done, you can access the website using StaticApp button on the top bar.
 
 '
