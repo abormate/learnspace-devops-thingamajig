577facc5c6e04ef458ab508e5bef6be9725aeac8
Abraham Immanuel R. Bormate <53473761+abormate@users.noreply.github.com>
2024-10-08 02:14:05 +0800

    DevOps LearnSpace -- Mirror Server Config

--- kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/19-02-rev-mirror-server.sh
+++ kodekloud-laboratories/kodekloud-engineer/linux-administration/level-02-linux/19-02-rev-mirror-server.sh
@@ -5,6 +5,6 @@
 # ------------------------------------------- #
 
 : '
-
+There is a static website running in Stratos Datacenter. They have already configured the app servers and code is already deployed there. To make it work properly, they need to configure LBR server. There are number of options for that, but team has decided to go with HAproxy. FYI, apache is running on port 3002 on all app servers. Complete this task as per below details.
 
 '
