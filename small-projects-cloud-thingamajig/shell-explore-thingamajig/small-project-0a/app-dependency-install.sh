84543ccca5dffccfdbcd879c7a1941b12b8cfa5b
Abraham Immanuel R. Bormate <abe.bormate@gmail.com>
2025-02-15 15:52:59 +0800

    02-2025-shell-write-install-dependency

--- small-projects-cloud-thingamajig/shell-explore-thingamajig/small-project-0a/app-dependency-install.sh
+++ small-projects-cloud-thingamajig/shell-explore-thingamajig/small-project-0a/app-dependency-install.sh
@@ -6,4 +6,6 @@
 
 OS_UPDATER_PATH="$( cd "$(dirname "${BASH_SOURCE[0]})" && pwd )"
 
+LSREGISTER_SERVICE="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister"
+
 # ------------------------------------------------ #
