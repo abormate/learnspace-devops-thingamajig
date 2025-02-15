5a483481f73e299089b46227619f6b70705113ce
Abraham Immanuel R. Bormate <abe.bormate@gmail.com>
2025-02-15 15:53:59 +0800

    02-2025-shell-write-install-dependency

--- small-projects-cloud-thingamajig/shell-explore-thingamajig/small-project-0a/app-dependency-install.sh
+++ small-projects-cloud-thingamajig/shell-explore-thingamajig/small-project-0a/app-dependency-install.sh
@@ -9,3 +9,8 @@ OS_UPDATER_PATH="$( cd "$(dirname "${BASH_SOURCE[0]})" && pwd )"
 LSREGISTER_SERVICE="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister"
 
 # ------------------------------------------------ #
+# common function
+write_log() {
+    echo $(date "+DATE: %Y-%m-%d%nTIME: %H:%M:%S"): "$@"
+    echo $(date "+DATE: %Y-%m-%d%nTIME: %H:%M:%S"): "$@" >> "${UPDATE_LOG_PATH}"
+}
