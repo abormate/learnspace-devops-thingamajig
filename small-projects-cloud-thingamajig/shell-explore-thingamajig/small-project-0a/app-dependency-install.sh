a251a084cf768fb00cd6025db05eeeeb0304d851
Abraham Immanuel R. Bormate <abe.bormate@gmail.com>
2025-02-15 15:50:40 +0800

    02-2025-shell-write-install-dependency

---
+++ small-projects-cloud-thingamajig/shell-explore-thingamajig/small-project-0a/app-dependency-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Shell script modular install software dependency
+
+# ------------------------------------------------ #
+# vars 
+
+OS_UPDATER_PATH="$( cd "$(dirname "${BASH_SOURCE[0]})" && pwd )"
+
+# ------------------------------------------------ #
